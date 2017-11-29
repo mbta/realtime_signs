9901f10b5e0bb31c66b000282edcf77173551141
Alex Sghia-Hughes <alex@mbtace.com>
2017-11-29 14:16:19 -0500

    start up the predictions getter

--- lib/sign/supervisor.ex
+++ lib/sign/supervisor.ex
@@ -7,6 +7,7 @@ defmodule Sign.Supervisor do
 
   def init(:ok) do
     children = [
+      worker(Sign.Predictions, [[name: Sign.Predictions]]),
       worker(Sign.State, [[name: Sign.State]]),
       worker(Sign.Stations.Live, [[name: Sign.Stations.Live, path: Application.get_env(:realtime_signs, :stations_config)]]),
       worker(Sign.Updater, [[name: Sign.Updater]])
