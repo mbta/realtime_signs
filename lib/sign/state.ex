9b0050b2eae280b42b78ebeb03ed42ef84dd85dc
Alex Sghia-Hughes <alex@mbtace.com>
2018-01-11 15:18:51 -0500

    add heartbeat messaging

--- lib/sign/state.ex
+++ lib/sign/state.ex
@@ -23,6 +23,7 @@ defmodule Sign.State do
   end
 
   def init(:ok) do
+    heartbeat()
     {:ok, %{}}
   end
 
@@ -38,6 +39,15 @@ defmodule Sign.State do
   """
   def update(pid \\ __MODULE__, trip_updates, vehicle_positions, current_time) do
     GenServer.call(pid, {:update, trip_updates, vehicle_positions, current_time})
+  end
+
+  def heartbeat(pid \\ __MODULE__) do
+    Process.send_after(self(), :heartbeat, 30_000)
+  end
+
+  def handle_call({:heartbeat}, _from, _sign_state) do
+    Logger.error("heart beat message")
+    Process.send_after(self(), :heartbeat, 30_000)
   end
 
   def handle_call({:update, %GTFS.Realtime.FeedMessage{entity: trip_updates}, %GTFS.Realtime.FeedMessage{entity: vehicle_positions}, current_time}, _from, sign_state) do
