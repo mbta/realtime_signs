b08c87499c671d402a2190f79f86e835aa8c4765
Alex Sghia-Hughes <alex@mbtace.com>
2018-01-24 11:30:14 -0500

    update prod startup and the bat files

--- realtime_signs_prod.bat
+++ realtime_signs_prod.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Git\bin\sh.exe" -c "cd ~/GitHub/realtime_signs; mix local.hex --force; mix deps.get --force; bash prod_startup.sh"
+"C:\Program Files\Git\bin\sh.exe" -c "cd ~/GitHub/realtime_signs; mix local.hex --force; mix local.rebar --force; mix deps.get --force; bash prod_startup.sh"
