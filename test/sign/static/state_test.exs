00abbb46e66d992253ad393c609966bc11e4bf7a
kohlerjp <john@mbtace.com>
2018-03-14 14:40:44 -0400

    add test files

---
+++ test/sign/static/state_test.exs
@@ -0,0 +1,29 @@
+defmodule Sign.Static.StateTest do
+  use ExUnit.Case
+  import Sign.Static.State
+  import ExUnit.CaptureLog
+  alias Sign.Station
+
+  @stations [
+    %Station{route_id: "Mattapan", stop_id: "MMIL"},
+    %Station{route_id: "Mattapan", stop_id: "RASH"}
+  ]
+
+  describe "static_station_codes/1" do
+    test "returns station codes for current static_stations" do
+      {:ok, static_signs} = start_supervised({Sign.Static.State, [refresh_time: 1, name: :test, stations: ["70262", "70268"]]})
+      static_station_codes = static_station_codes(static_signs)
+      assert Enum.count(static_station_codes) == 2
+      assert "MMIL" in static_station_codes
+      assert "RASH" in static_station_codes
+    end
+
+    test "refreshes sign according to refresh_time" do
+      log = capture_log [level: :info], fn ->
+        start_supervised({Sign.Static.State, [refresh_time: 1, name: :refresh_test, stations: ["70268"]]})
+        Process.sleep(40)
+      end
+      assert log =~ "MMIL :: [%Sign.Message{duration: 180, message: [{\"Mattapan  Welcome!\", nil}], placement: [\"s1\"], when: nil}]"
+    end
+  end
+end
