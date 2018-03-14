7cadd5fcb68e6b5e0a69b1e26eb9f86066ffc879
kohlerjp <john@mbtace.com>
2018-03-14 14:57:51 -0400

    splunk in dev backend

--- config/dev.exs
+++ config/dev.exs
@@ -1,5 +1,8 @@
 use Mix.Config
 
+config :logger,
+  backends: [{Logger.Backend.Splunk, :splunk}, :console]
+
 config :logger, :splunk,
   connector: Logger.Backend.Splunk.Output.Http,
   host: 'https://http-inputs-mbta.splunkcloud.com/services/collector/event',
