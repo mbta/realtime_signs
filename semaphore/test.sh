02d509b226e310f13b6373043ffb2d00f2c7ae05
kohlerjp <john@mbtace.com>
2018-03-14 17:34:34 -0400

    hold plts in cache

--- semaphore/test.sh
+++ semaphore/test.sh
@@ -3,4 +3,12 @@ set -e
 
 MIX_ENV=test mix coveralls.json
 bash <(curl -s https://codecov.io/bash) -t $REALTIME_SIGNS_CODECOV_TOKEN
+
+# copy any pre-built PLTs to the right directory
+find $SEMAPHORE_CACHE_DIR -name "dialyxir_*_deps-dev.plt*" | xargs -I{} cp '{}' _build/test
+
+MIX_ENV=test mix dialyzer --plt
+# copy build PLTs back
+cp _build/test/*_deps-dev.plt* $SEMAPHORE_CACHE_DIR
+
 MIX_ENV=test mix dialyzer --halt-exit-status
