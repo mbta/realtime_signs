ed538cebf7f4091b2638c3f6654151cb931fe26f
kohlerjp <john@mbtace.com>
2018-03-14 14:44:55 -0400

    reconfigure logger

--- config/dev.exs
+++ config/dev.exs
@@ -1,1 +1,8 @@
 use Mix.Config
+
+config :logger, :splunk,
+  connector: Logger.Backend.Splunk.Output.Http,
+  host: 'https://http-inputs-mbta.splunkcloud.com/services/collector/event',
+  token: {:system, "STAGING_SIGNS_SPLUNK_TOKEN"},
+  format: "$dateT$time [$level]$levelpad $metadata$message\n",
+  metadata: [:request_id]
