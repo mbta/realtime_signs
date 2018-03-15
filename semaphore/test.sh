eed295dfc1ec665985cea5b49b1d5a872b42e25e
kohlerjp <john@mbtace.com>
2018-03-15 09:44:36 -0400

    create build dir

--- semaphore/test.sh
+++ semaphore/test.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 set -e
+set -x
 
 MIX_ENV=test mix coveralls.json
 bash <(curl -s https://codecov.io/bash) -t $REALTIME_SIGNS_CODECOV_TOKEN
 
 # copy any pre-built PLTs to the right directory
+mkdir -p _build/test/
 find $SEMAPHORE_CACHE_DIR -name "dialyxir_*_deps-dev.plt*" | xargs -I{} cp '{}' _build/test
 
 MIX_ENV=test mix dialyzer --plt
 # copy build PLTs back
-cp _build/test/*_deps-dev.plt* $SEMAPHORE_CACHE_DIR
+#cp _build/test/*_deps-dev.plt* $SEMAPHORE_CACHE_DIR
 
 MIX_ENV=test mix dialyzer --halt-exit-status
