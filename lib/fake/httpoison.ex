5097b0cc74141ebd195680fd5c78a5c2fed90070
kohlerjp <john@mbtace.com>
2018-03-19 11:31:22 -0400

    Use fake http client

--- lib/fake/httpoison.ex
+++ lib/fake/httpoison.ex
@@ -6,9 +6,17 @@ defmodule Fake.HTTPoison do
       send options[:stream_to], %HTTPoison.AsyncEnd{}
     end
     {url, headers, options}
+    mock_response(url)
   end
 
   def post(url, body, headers \\ []) do
     {url, body, headers}
   end
+
+  def mock_response("https://api-v3.mbta.com/schedules" <> _) do
+    json = %{"data" => []}
+    encoded = Poison.encode!(json)
+    {:ok, %HTTPoison.Response{body: encoded}}
+  end
+  def mock_response(_), do: {:ok, %HTTPoison.Response{body: ""}}
 end
