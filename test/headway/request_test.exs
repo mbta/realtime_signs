543f3b983d1bdcd56a7b5e750ddc7954e441970d
kohlerjp <john@mbtace.com>
2018-03-19 15:32:43 -0400

    Add tests for headway modules

---
+++ test/headway/request_test.exs
@@ -0,0 +1,31 @@
+defmodule Headway.RequestTest do
+  use ExUnit.Case, async: true
+  require Logger
+  import ExUnit.CaptureLog
+  import Headway.Request
+
+  describe "get_schedules/1" do
+    test "gracefully handles bad responses and logs warning" do
+      log = capture_log [level: :warn], fn ->
+        assert get_schedules(["500_error"]) == []
+        assert get_schedules(["unknown_error"]) == []
+      end
+
+      assert log =~ "Received 500 status code from API"
+      assert log =~ "Could not load schedules"
+      assert log =~ "Bad URL"
+    end
+  end
+
+  test "Logs warning when json data cannot be parsed" do
+      log = capture_log [level: :warn], fn ->
+        assert get_schedules(["parse_error"]) == []
+      end
+
+      assert log =~ "Could not decode response:"
+  end
+
+  test "parses valid json" do
+    assert get_schedules(["valid_json"]) == [%{"relationships" => "trip"}]
+  end
+end
