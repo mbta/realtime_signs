97e0a220a24ca6b66e4ca45d4d5bb14dca06512b
kohlerjp <john@mbtace.com>
2018-03-20 10:25:44 -0400

    use built test deps

--- semaphore/test.sh
+++ semaphore/test.sh
@@ -10,6 +10,6 @@ find $SEMAPHORE_CACHE_DIR -name "dialyxir_*_deps-dev.plt*" | xargs -I{} cp '{}' 
 
 MIX_ENV=test mix dialyzer --plt
 # copy build PLTs back
-cp _build/test/*_deps-dev.plt* $SEMAPHORE_CACHE_DIR
+cp _build/test/*_deps-test.plt* $SEMAPHORE_CACHE_DIR
 
 MIX_ENV=test mix dialyzer --halt-exit-status
