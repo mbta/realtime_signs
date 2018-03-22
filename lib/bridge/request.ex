b3b79fc2705211f7c5a1f8da0f6d7127ff0831b8
kohlerjp <john@mbtace.com>
2018-03-22 16:15:07 -0400

    add specs

--- lib/bridge/request.ex
+++ lib/bridge/request.ex
@@ -3,6 +3,7 @@ defmodule Bridge.Request do
 
   @bridge_url "https://slg.aecomonline.net/api/v1/bridge/"
 
+  @spec get_status(non_neg_integer) :: String.t | nil
   def get_status(id) do
     headers = [{"Authorization", get_auth_header()}]
     http_client = Application.get_env(:realtime_signs, :http_client)
--- lib/sign/static/messages.ex
+++ lib/sign/static/messages.ex
@@ -1,11 +1,13 @@
 defmodule Sign.Static.Messages do
   alias Sign.{Message, Content, Station}
   alias Sign.Static.Text
+  alias Headway.ScheduleHeadway
 
   @additional_duration 60
   @sl3_route_id "743"
 
   @doc "Returns a Content struct representing the static messages that will be displayed for given stations"
+  @spec station_messages([Station.t], integer, %{Station.id => ScheduleHeadway.t}, DateTime.t, String.t | nil) :: [Content.t]
   def station_messages(stations, refresh_rate, headways, current_time, bridge_status) do
     bridge_raised? = Bridge.Chelsea.raised?(bridge_status)
     stations
@@ -13,11 +15,13 @@ defmodule Sign.Static.Messages do
     |> Enum.map(&build_content(&1, refresh_rate, headways, current_time, bridge_raised?))
   end
 
+  @spec build_content({Station.t, 1 | 0}, integer, map, DateTime.t, boolean) :: Content.t
   defp build_content({station, direction}, refresh_rate, headways, current_time, bridge_raised?) do
     message_text = get_text(Map.get(headways, station.id), station.route_id, current_time, bridge_raised?)
     %Content{station: station.sign_id, messages: build_messages(station, direction, refresh_rate, message_text)}
   end
 
+  @spec build_messages(Station.t, 1 | 0, integer, Sign.Static.Text.t) :: [Message.t]
   defp build_messages(station, direction, refresh_rate, {text_top, text_bottom}) do
     duration = message_duration(refresh_rate)
     message_line_top = build_message(station, direction, text_top, duration, :top)
@@ -25,29 +29,35 @@ defmodule Sign.Static.Messages do
     [message_line_top, message_line_bottom]
   end
 
+  @spec build_message(Station.t, 1 | 0, String.t, integer, :top | :bottom) :: Message.t
   defp build_message(station, direction, text, duration, line_value) do
     placement = placement_code(station, direction, Sign.Message.line_code(line_value))
     %Message{placement: placement, message: [{text, nil}], duration: duration}
   end
 
+  @spec message_duration(integer) :: integer
   defp message_duration(refresh_rate_milliseconds) do
     refresh_rate_seconds = div(refresh_rate_milliseconds, 1_000)
     refresh_rate_seconds + additional_duration()
   end
 
+  @spec placement_code(Station.t, 1 | 0, String.t) :: [String.t]
   defp placement_code(station, direction, line_placement) do
     sign_label = Map.get(station.zones, direction)
     [Message.sign_code(sign_label) <> line_placement]
   end
 
+  @spec additional_duration() :: integer
   defp additional_duration() do
     if Mix.env == :test, do: 0, else: @additional_duration
   end
 
+  @spec station_with_zones(Station.t) :: [{Station.t, 1 | 0}]
   defp station_with_zones(station) do
     Enum.map(Station.zone_ids(station), &{station, &1})
   end
 
+  @spec get_text(ScheduleHeadway.t, String.t, DateTime.t, boolean) :: Text.t
   defp get_text(headway, route_id, current_time, bridge_raised?) do
     if bridge_raised? and route_id == @sl3_route_id do
       Text.text_for_raised_bridge()
