2bb6977a74de79d11b6a5ef06108d8c131a28d77
Gabe Durazo <gabe@mbtace.com>
2018-04-27 15:01:10 -0500

    Update NextTrainCountdown to handle arrives/departs

--- lib/content/audio/next_train_countdown.ex
+++ lib/content/audio/next_train_countdown.ex
@@ -3,28 +3,31 @@ defmodule Content.Audio.NextTrainCountdown do
   The next train to [destination] arrives in [n] minutes.
   """
 
-  @enforce_keys [:destination, :minutes]
+  @enforce_keys [:destination, :verb, :minutes]
   defstruct @enforce_keys
+
+  @type verb :: :arrives | :departs
 
   @type t :: %__MODULE__{
     destination: :ashmont | :mattapan,
+    verb: verb(),
     minutes: integer()
   }
 
   require Logger
 
-  @spec from_predictions_message(Content.Message.t()) :: t() | nil
-  def from_predictions_message(%Content.Message.Predictions{minutes: n, headsign: "Ashmont"}) when is_integer(n) do
-    %__MODULE__{destination: :ashmont, minutes: n}
+  @spec from_predictions_message(Content.Message.t(), verb()) :: t() | nil
+  def from_predictions_message(%Content.Message.Predictions{minutes: n, headsign: "Ashmont"}, verb) when is_integer(n) do
+    %__MODULE__{destination: :ashmont, minutes: n, verb: verb}
   end
-  def from_predictions_message(%Content.Message.Predictions{minutes: n, headsign: "Mattapan"}) when is_integer(n) do
-    %__MODULE__{destination: :mattapan, minutes: n}
+  def from_predictions_message(%Content.Message.Predictions{minutes: n, headsign: "Mattapan"}, verb) when is_integer(n) do
+    %__MODULE__{destination: :mattapan, minutes: n, verb: verb}
   end
-  def from_predictions_message(%Content.Message.Predictions{minutes: n, headsign: headsign}) when is_integer(n) do
+  def from_predictions_message(%Content.Message.Predictions{minutes: n, headsign: headsign}, _verb) when is_integer(n) do
     Logger.warn("Content.Audio.NextTrainCountdown.from_predictions_message: unknown headsign: #{headsign}")
     nil
   end
-  def from_predictions_message(_) do
+  def from_predictions_message(_, _verb) do
     nil
   end
 
@@ -32,13 +35,14 @@ defmodule Content.Audio.NextTrainCountdown do
     alias PaEss.Utilities
 
     def to_params(audio) do
-      {"90", [destination_var(audio), arrives_in_var(), minutes_var(audio)], :audio}
+      {"90", [destination_var(audio), verb_var(audio), minutes_var(audio)], :audio}
     end
 
     defp destination_var(%{destination: :ashmont}), do: "4016"
     defp destination_var(%{destination: :mattapan}), do: "4100"
 
-    defp arrives_in_var(), do: "503"
+    defp verb_var(%{verb: :arrives}), do: "503"
+    defp verb_var(%{verb: :departs}), do: "502"
 
     defp minutes_var(%{minutes: minutes}) do
       Utilities.countdown_minutes_var(minutes)
