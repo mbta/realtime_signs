ec0b84c863979a2fdbd53fca602f1b1821ce3c88
Alex Sghia-Hughes <alex@mbtace.com>
2018-04-30 12:15:52 -0400

    fake headway generater

---
+++ lib/fake/headway/request.ex
@@ -0,0 +1,17 @@
+defmodule Fake.Headway.Request do
+  alias Headway.ScheduleHeadway
+
+  @times [
+    ~N[2017-07-04 09:05:00],
+    ~N[2017-07-04 08:55:00],
+    ~N[2017-07-04 08:45:00],
+    ~N[2017-07-04 09:20:00]
+  ]
+
+  def get_schedules(["123"]) do
+    Enum.map(@times, fn time ->
+      %{"relationships" => %{"stop" => %{"data" => %{"id" => "123"}}},
+        "attributes" => %{"departure_time" => Timex.format!(Timex.to_datetime(time, "America/New_York"), "{ISO:Extended}")}}
+    end)
+  end
+end
