41fc90884b2ec403d9e035ca77e6766f071403f0
Alex Sghia-Hughes <alex@mbtace.com>
2018-04-30 14:11:09 -0400

    update tests

--- test/engine/headways_test.exs
+++ test/engine/headways_test.exs
@@ -2,7 +2,7 @@ defmodule Engine.HeadwaysTest do
   use ExUnit.Case
   describe "register callback" do
     test "adds the stop id to the state" do
-      assert Engine.Headways.handle_call({:register, "123"}, %{}, %{}) == {:reply, %{"123" => []}, %{"123" => []}}
+      assert Engine.Headways.handle_call({:register, "123"}, %{}, %{}) == {:reply, :ok, %{"123" => []}}
     end
   end
 
