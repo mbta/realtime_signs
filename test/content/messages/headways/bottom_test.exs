0ea752da8840e94fbde27ef4dd37a72921b4f91b
Alex Sghia-Hughes <alex@mbtace.com>
2018-05-02 16:22:14 -0400

    add a content message test

--- test/content/messages/headways/bottom_test.exs
+++ test/content/messages/headways/bottom_test.exs
@@ -11,5 +11,10 @@ defmodule Content.Message.Headways.BottomTest do
       assert Content.Message.to_string(%Content.Message.Headways.Bottom{range: {2, 2}}) ==
         "Every 2 min"
     end
+
+    test "when the message is a first departure message, still shows the range" do
+      assert Content.Message.to_string(%Content.Message.Headways.Bottom{range: {:first_departure, {2, 2}, Timex.now()}}) ==
+        "Every 2 min"
+    end
   end
 end
