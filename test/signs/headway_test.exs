6972a0e55550a6a01c5b246f8f1ff64c9428314a
Alex Sghia-Hughes <asghiah@gmail.com>
2018-05-03 10:50:31 -0400

    Ash first departure (#61)
    * wip make the signs work with first departures
    
    * handle first departures
    
    * compiler warnings
    
    * add a content message test

--- test/signs/headway_test.exs
+++ test/signs/headway_test.exs
@@ -40,12 +40,60 @@ defmodule Signs.HeadwayTest do
       log = capture_log [level: :info], fn ->
         handle_info(:update_content, sign)
       end
-      assert log != "update_sign called"
+      refute log =~ "update_sign called"
+    end
+
+    test "when the first departure is in the future, does not send an update" do
+      sign = %Signs.Headway{
+        id: "SIGN",
+        pa_ess_id: "1",
+        gtfs_stop_id: "first_departure",
+        route_id: "743",
+        headsign: "Chelsea",
+        headway_engine: FakeHeadwayEngine,
+        sign_updater: FakeSignUpdater,
+        timer: nil,
+        current_content_top: %Content.Message.Headways.Top{headsign: "Chelsea", vehicle_type: :bus},
+        current_content_bottom: %Content.Message.Headways.Bottom{range: {1, 3}}
+      }
+
+      log = capture_log [level: :info], fn ->
+        handle_info(:update_content, sign)
+      end
+      refute log =~ "update_sign called"
+    end
+
+    test "when the first departure is in the future but within the range of the headway, sends an update" do
+      sign = %Signs.Headway{
+        id: "SIGN",
+        pa_ess_id: "1",
+        gtfs_stop_id: "first_departure_soon",
+        route_id: "743",
+        headsign: "Chelsea",
+        headway_engine: FakeHeadwayEngine,
+        sign_updater: FakeSignUpdater,
+        timer: nil,
+        current_content_top: %Content.Message.Headways.Top{headsign: "Chelsea", vehicle_type: :bus},
+        current_content_bottom: %Content.Message.Headways.Bottom{range: {1, 3}}
+      }
+
+      log = capture_log [level: :info], fn ->
+        handle_info(:update_content, sign)
+      end
+      assert log =~ "update_sign called"
     end
   end
 end
 
 defmodule FakeHeadwayEngine do
+  def get_headways("first_departure_soon") do
+    future_departure = Timex.shift(Timex.now(), minutes: 5)
+    {:first_departure, {8, 10}, future_departure}
+  end
+  def get_headways("first_departure") do
+    future_departure = Timex.shift(Timex.now(), minutes: 10)
+    {:first_departure, {1, 2}, future_departure}
+  end
   def get_headways(_stop_id) do
     {1, 2}
   end
