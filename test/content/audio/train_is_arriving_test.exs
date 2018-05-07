9ed950b230ffbbdd91b8f1dcc2906cc1bd2ad989
Gabe Durazo <gabe@mbtace.com>
2018-05-07 13:19:25 -0500

    Change train-is-now-arriving messages to be both audio+visual

--- test/content/audio/train_is_arriving_test.exs
+++ test/content/audio/train_is_arriving_test.exs
@@ -5,12 +5,12 @@ defmodule Content.Audio.TrainIsArrivingTest do
   describe "Content.Audio.to_params protocol" do
     test "Next train to Ashmont is now arriving" do
       audio = %Content.Audio.TrainIsArriving{destination: :ashmont}
-      assert Content.Audio.to_params(audio) == {"90129", [], :audio}
+      assert Content.Audio.to_params(audio) == {"90129", [], :audio_visual}
     end
 
     test "Next train to Mattapan is now arriving" do
       audio = %Content.Audio.TrainIsArriving{destination: :mattapan}
-      assert Content.Audio.to_params(audio) == {"90128", [], :audio}
+      assert Content.Audio.to_params(audio) == {"90128", [], :audio_visual}
     end
   end
 
