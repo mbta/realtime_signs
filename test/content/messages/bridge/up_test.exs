39c64ca7565e5914603b369b85f8ec369445000f
Alex Sghia-Hughes <alex@mbtace.com>
2018-05-08 11:59:21 -0400

    test coverage

--- test/content/messages/bridge/up_test.exs
+++ test/content/messages/bridge/up_test.exs
@@ -3,6 +3,7 @@ defmodule Content.Message.Bridge.UpTest do
 
   describe "to_string/1" do
     test "says bridge is up" do
+      msg = Content.Message.Bridge.Up.new()
       assert Content.Message.to_string(%Content.Message.Bridge.Up{}) == "Bridge is up"
     end
   end
