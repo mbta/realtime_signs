8e107762e95e3ae150acca5de34e9ca3532abde6
Alex Sghia-Hughes <alex@mbtace.com>
2018-05-08 13:59:57 -0400

    update module doc

--- lib/content/message/bridge/delays.ex
+++ lib/content/message/bridge/delays.ex
@@ -1,6 +1,6 @@
 defmodule Content.Message.Bridge.Delays do
   @moduledoc """
-  A static message for a sign, for use in sending arbitrary text for announcements
+  A message for use when the SL3 is delayed by a bridge.
   """
 
   defstruct []
--- lib/content/message/bridge/up.ex
+++ lib/content/message/bridge/up.ex
@@ -1,6 +1,6 @@
 defmodule Content.Message.Bridge.Up do
   @moduledoc """
-  A static message for a sign, for use in sending arbitrary text for announcements
+  A message to be used when the route is affected by a bridge being up
   """
 
   defstruct []
