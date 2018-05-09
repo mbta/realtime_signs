eeed034ee0d73144c5c839210b5c3298a856aaf1
David Maltzan <dmaltzan@mbta.com>
2018-05-09 18:54:53 -0400

    pass in current_time

--- test/bridge/request_test.exs
+++ test/bridge/request_test.exs
@@ -4,9 +4,14 @@ defmodule Bridge.RequestTest do
   import ExUnit.CaptureLog
 
   describe "get_status/1" do
+    test "parses valid response" do
+      {:ok, test_time} = "2000-01-23T04:51:07.000+00:00" |> Timex.parse("{ISO:Extended}")
+      assert get_status(1, test_time) == {"Raised", 5}
+    end
+
     test "Logs warning with bad status code" do
       log = capture_log [level: :warn], fn ->
-        refute get_status(500)
+        refute get_status(500, Timex.now())
       end
 
       assert log =~ "Could not query bridge API: status code 500"
@@ -14,7 +19,7 @@ defmodule Bridge.RequestTest do
 
     test "Logs warning when request fails" do
       log = capture_log [level: :warn], fn ->
-        refute get_status(754)
+        refute get_status(754, Timex.now())
       end
 
       assert log =~ "Could not query bridge API: Unknown error"
@@ -22,7 +27,7 @@ defmodule Bridge.RequestTest do
 
     test "Logs warning when parsing fails" do
       log = capture_log [level: :warn], fn ->
-        refute get_status(201)
+        refute get_status(201, Timex.now())
       end
 
       assert log =~ "Could not parse json response"
