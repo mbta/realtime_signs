a4d070c86aba8b141ff80bd12f4a84dfe9b8b8fd
Gabe Durazo <gabe@mbtace.com>
2018-05-11 10:34:13 -0500

    Clear up warnings

--- test/content/messages/bridge/delays_test.exs
+++ test/content/messages/bridge/delays_test.exs
@@ -4,7 +4,7 @@ defmodule Content.Message.Bridge.DelaysTest do
   describe "to_string/1" do
     test "says expect delays" do
       msg = Content.Message.Bridge.Delays.new()
-      assert Content.Message.to_string(%Content.Message.Bridge.Delays{}) == "Expect SL3 delays"
+      assert Content.Message.to_string(msg) == "Expect SL3 delays"
     end
   end
 end
