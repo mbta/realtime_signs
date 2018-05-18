d292e1521de2dcda21ae195e5e88878a6efc6fac
Alex Sghia-Hughes <alex@mbtace.com>
2018-05-18 14:05:19 -0400

    spacing again

--- lib/fake/external_config/local.ex
+++ lib/fake/external_config/local.ex
@@ -2,14 +2,13 @@ defmodule Fake.ExternalConfig.Local do
   @behaviour ExternalConfig.Interface
 
   @impl ExternalConfig.Interface
- def get("unchanged") do
-   :unchanged
- end
- def get(_current_version) do
-   {nil, %{"chelsea_inbound" => %{"enabled" => true},
-    "chelsea_outbound" => %{"enabled" => false},
-    "MVAL0" => %{"enabled" => false}
-    }}
- end
-
+  def get("unchanged") do
+    :unchanged
+  end
+  def get(_current_version) do
+    {nil, %{"chelsea_inbound" => %{"enabled" => true},
+     "chelsea_outbound" => %{"enabled" => false},
+     "MVAL0" => %{"enabled" => false}
+     }}
+  end
 end
