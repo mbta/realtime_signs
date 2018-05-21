b09c6db7a155c99cd1c7af874f78f16216e4f8a6
Alex Sghia-Hughes <asghiah@gmail.com>
2018-05-21 16:24:57 -0400

    Ash gov center (#84)
    * add audio stuff
    
    * type change
    
    * missed tests

--- test/content/audio/next_train_countdown_test.exs
+++ test/content/audio/next_train_countdown_test.exs
@@ -20,6 +20,24 @@ defmodule Content.Audio.NextTrainCountdownTest do
       }
       assert Content.Audio.to_params(audio) == {"90", ["4100", "503", "5005"], :audio}
     end
+
+    test "Next train to Bowdoin" do
+      audio = %Content.Audio.NextTrainCountdown{
+        destination: :bowdoin,
+        verb: :arrives,
+        minutes: 5,
+      }
+      assert Content.Audio.to_params(audio) == {"90", ["4055", "503", "5005"], :audio}
+    end
+
+    test "Next train to Wonderland" do
+      audio = %Content.Audio.NextTrainCountdown{
+        destination: :wonderland,
+        verb: :arrives,
+        minutes: 5,
+      }
+      assert Content.Audio.to_params(audio) == {"90", ["4044", "503", "5005"], :audio}
+    end
   end
 
   describe "from_predictions_message/1" do
@@ -33,6 +51,18 @@ defmodule Content.Audio.NextTrainCountdownTest do
       message = %Content.Message.Predictions{headsign: "Mattapan", minutes: 5}
       assert Content.Audio.NextTrainCountdown.from_predictions_message(message, :arrives) ==
         %Content.Audio.NextTrainCountdown{destination: :mattapan, verb: :arrives, minutes: 5}
+    end
+
+    test "Converts Wonderland countdown message to audio" do
+      message = %Content.Message.Predictions{headsign: "Wonderland", minutes: 5}
+      assert Content.Audio.NextTrainCountdown.from_predictions_message(message, :arrives) ==
+        %Content.Audio.NextTrainCountdown{destination: :wonderland, verb: :arrives, minutes: 5}
+    end
+
+    test "Converts Bowdoin countdown message to audio" do
+      message = %Content.Message.Predictions{headsign: "Bowdoin", minutes: 5}
+      assert Content.Audio.NextTrainCountdown.from_predictions_message(message, :arrives) ==
+        %Content.Audio.NextTrainCountdown{destination: :bowdoin, verb: :arrives, minutes: 5}
     end
 
     test "Logs unknown headsign countdown message" do
