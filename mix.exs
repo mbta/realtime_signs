da6fcbd0c0f96981fd9aa9f6f5b25f3f3451d5fb
kohlerjp <john@mbtace.com>
2018-05-22 11:41:32 -0400

    update sentry

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule RealtimeSigns.Mixfile do
       {:httpoison, "~> 1.0"},
       {:logger_splunk_backend, git: "https://github.com/mbta/logger_splunk_backend.git"},
       {:poison, "~> 3.1"},
-      {:sentry, "~> 6.0.0"},
+      {:sentry, "~> 6.2.1"},
       {:timex, "~> 3.1.0"},
       {:inflex, "~> 1.8.1"}
     ]
