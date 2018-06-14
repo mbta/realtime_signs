ef8ee50a9e0052fd21190986ef6958d7f9c6093f
kohlerjp <john@mbtace.com>
2018-06-14 14:50:09 -0400

    add new tests

--- test/engine/predictions_test.exs
+++ test/engine/predictions_test.exs
@@ -16,6 +16,18 @@ defmodule Engine.PredictionsTest do
       assert updated_state == existing_state
     end
 
+    test "updates vehicle positions in table" do
+      trip_update_url = Application.get_env(:realtime_signs, :trip_update_url)
+      position_url = Application.get_env(:realtime_signs, :vehicle_position_url)
+      Application.put_env(:realtime_signs, :trip_update_url, "trip_updates_304")
+      Application.put_env(:realtime_signs, :vehicle_positions_url, "vehicle_positions_url")
+      existing_state = {~N[2017-07-04 09:05:00], ~N[2017-07-04 09:05:00]}
+      {:noreply, updated_state} = handle_info(:update, existing_state)
+      Application.put_env(:realtime_signs, :trip_update_url, trip_update_url)
+      Application.put_env(:realtime_signs, :vehicle_position_url, position_url)
+      assert updated_state == existing_state
+    end
+
     test "logs error when invalid HTTP response returned" do
       trip_update_url = Application.get_env(:realtime_signs, :trip_update_url)
       position_url = Application.get_env(:realtime_signs, :vehicle_position_url)
@@ -50,18 +62,17 @@ defmodule Engine.PredictionsTest do
   end
 
   describe "currently_boarding/1" do
-    test "returns correct predictions for stop" do
+    test "returns true when vehicle is boarding" do
       prediction = %Predictions.Prediction{
         stop_id: "stop_1",
-        seconds_until_arrival: 45,
+        seconds_until_arrival: 15,
         direction_id: 1,
         route_id: "Blue",
       }
-      prediction_map = %{{"stop_1", 1} => [prediction]}
-      table_id = :ets.new(:predictions_engine_test, [:set, :protected, read_concurrency: true])
-      :ets.insert(table_id, Enum.into(prediction_map, []))
-      assert for_stop(table_id, "stop_1", 1) == [prediction]
-      assert for_stop(table_id, "no_entry", 0) == []
+      table_id = :ets.new(:predictions_engine_positions, [:set, :protected, read_concurrency: true])
+      :ets.insert(table_id, [{"stop_1", true}])
+      assert currently_boarding?(table_id,"stop_1")
+      refute currently_boarding?(table_id, "stop_2")
     end
   end
 end
---
+++ test/positions/positions_test.exs
@@ -0,0 +1,35 @@
+defmodule Positions.PositionsTest do
+  use ExUnit.Case, async: true
+  import Positions.Positions
+
+  @feed_message %GTFS.Realtime.FeedMessage{entity: [%GTFS.Realtime.FeedEntity{
+                 alert: nil, id: "1528987657_G-10131", is_deleted: false, trip_update: nil,
+                 vehicle: %GTFS.Realtime.VehiclePosition{congestion_level: nil,
+                   current_status: :STOPPED_AT, current_stop_sequence: 1, occupancy_status: nil,
+                   position: %GTFS.Realtime.Position{
+                     bearing: 135.0, latitude: 42.340179443359375, longitude: -71.1670913696289,
+                     odometer: nil, speed: nil
+                   },
+                  stop_id: "70106", timestamp: 1528987657, trip: %GTFS.Realtime.TripDescriptor{
+                    direction_id: 1, route_id: "Green-B", schedule_relationship: :SCHEDULED,
+                    start_date: "20180614", start_time: nil, trip_id: "36418561"
+                  },
+                vehicle: %GTFS.Realtime.VehicleDescriptor{
+                  id: "G-10131", label: "3841-3625", license_plate: nil }
+              }
+            }], header: %GTFS.Realtime.FeedHeader{gtfs_realtime_version: "1.0",
+          incrementality: :FULL_DATASET, timestamp: 1490783458}}
+
+  describe "parse_pb_response/1" do
+    test "parses response" do
+      encoded_pb = GTFS.Realtime.FeedMessage.encode(@feed_message)
+      assert parse_pb_response(encoded_pb) == @feed_message
+    end
+  end
+
+  describe "get_all/1" do
+    test "gets all stations" do
+      assert get_all(@feed_message) == [{"70106", true}]
+    end
+  end
+end
