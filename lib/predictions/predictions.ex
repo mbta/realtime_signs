af13093ded32d76dee6af4fedaccee43686886f3
David Maltzan <dmaltzan@mbta.com>
2018-07-02 17:31:58 -0400

    a little more logging

--- lib/predictions/predictions.ex
+++ lib/predictions/predictions.ex
@@ -19,13 +19,19 @@ defmodule Predictions.Predictions do
     current_time_seconds = DateTime.to_unix(current_time)
     prediction_time = if stop_time_update.departure, do: stop_time_update.departure, else: stop_time_update.arrival
 
-    %Prediction{
-      stop_id: stop_time_update.stop_id,
-      direction_id: direction_id,
-      seconds_until_arrival: max(0, prediction_time.time - current_time_seconds),
-      route_id: route_id,
-      headsign: headsign_for_prediction(route_id, direction_id, last_stop_id)
-    }
+    case headsign_for_prediction(route_id, direction_id, last_stop_id) do
+      {:ok, headsign} ->
+        %Prediction{
+                     stop_id: stop_time_update.stop_id,
+                     direction_id: direction_id,
+                     seconds_until_arrival: max(0, prediction_time.time - current_time_seconds),
+                     route_id: route_id,
+                     headsign: headsign
+                   }
+      {:error, :not_found} ->
+        Logger.error("Could not find headsign for stop_time_update #{inspect stop_time_update} with last_stop_id #{last_stop_id} on route #{route_id} in direction #{direction_id}")
+        nil
+    end
   end
 
   def parse_pb_response(body) do
@@ -37,26 +43,23 @@ defmodule Predictions.Predictions do
     Enum.sort(predictions, & &1.seconds_until_arrival < &2.seconds_until_arrival)
   end
 
-  @spec headsign_for_prediction(String.t(), 0 | 1, String.t()) :: String.t()
-  defp headsign_for_prediction("Mattapan", 0, _), do: "Mattapan"
-  defp headsign_for_prediction("Mattapan", 1, _), do: "Ashmont"
-  defp headsign_for_prediction("Orange", 0, _), do: "Frst Hills"
-  defp headsign_for_prediction("Orange", 1, _), do: "Oak Grove"
-  defp headsign_for_prediction("Blue", 0, _), do: "Bowdoin"
-  defp headsign_for_prediction("Blue", 1, _), do: "Wonderland"
-  defp headsign_for_prediction("Red", 1, _), do: "Alewife"
-  defp headsign_for_prediction("Red", 0, last_stop_id) when last_stop_id in ["70087", "70089", "70091", "70093"], do: "Ashmont"
-  defp headsign_for_prediction("Red", 0, last_stop_id) when last_stop_id in ["70097", "70101", "70103", "70105"], do: "Braintree"
-  defp headsign_for_prediction("Green-B", 0, _), do: "Boston Col"
-  defp headsign_for_prediction("Green-C", 0, _), do: "Clvlnd Cir"
-  defp headsign_for_prediction("Green-D", 0, _), do: "Riverside"
-  defp headsign_for_prediction("Green-E", 0, _), do: "Heath St"
-  defp headsign_for_prediction(_, 1, "70209"), do: "Lechmere"
-  defp headsign_for_prediction(_, 1, "70205"), do: "North Sta"
-  defp headsign_for_prediction(_, 1, "70201"), do: "Govt Ctr"
-  defp headsign_for_prediction(_, 1, "70200"), do: "Park St"
-  defp headsign_for_prediction(route_id, direction_id, last_stop_id) do
-    Logger.error("Could not find headsign for route_id #{route_id}, #{direction_id}, #{last_stop_id}")
-    ""
-  end
+  @spec headsign_for_prediction(String.t(), 0 | 1, String.t()) :: {:ok, String.t()} | {:error, :not_found}
+  defp headsign_for_prediction("Mattapan", 0, _), do: {:ok, "Mattapan"}
+  defp headsign_for_prediction("Mattapan", 1, _), do: {:ok, "Ashmont"}
+  defp headsign_for_prediction("Orange", 0, _), do: {:ok, "Frst Hills"}
+  defp headsign_for_prediction("Orange", 1, _), do: {:ok, "Oak Grove"}
+  defp headsign_for_prediction("Blue", 0, _), do: {:ok, "Bowdoin"}
+  defp headsign_for_prediction("Blue", 1, _), do: {:ok, "Wonderland"}
+  defp headsign_for_prediction("Red", 1, _), do: {:ok, "Alewife"}
+  defp headsign_for_prediction("Red", 0, last_stop_id) when last_stop_id in ["70087", "70089", "70091", "70093"], do: {:ok, "Ashmont"}
+  defp headsign_for_prediction("Red", 0, last_stop_id) when last_stop_id in ["70097", "70101", "70103", "70105"], do: {:ok, "Braintree"}
+  defp headsign_for_prediction("Green-B", 0, _), do: {:ok, "Boston Col"}
+  defp headsign_for_prediction("Green-C", 0, _), do: {:ok, "Clvlnd Cir"}
+  defp headsign_for_prediction("Green-D", 0, _), do: {:ok, "Riverside"}
+  defp headsign_for_prediction("Green-E", 0, _), do: {:ok, "Heath St"}
+  defp headsign_for_prediction(_, 1, "70209"), do: {:ok, "Lechmere"}
+  defp headsign_for_prediction(_, 1, "70205"), do: {:ok, "North Sta"}
+  defp headsign_for_prediction(_, 1, "70201"), do: {:ok, "Govt Ctr"}
+  defp headsign_for_prediction(_, 1, "70200"), do: {:ok, "Park St"}
+  defp headsign_for_prediction(_, _, _), do: {:error, :not_found}
 end
