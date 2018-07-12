231998c9ea50f72b29dea5f62b78d2b80bfa2d2c
Alex Sghia-Hughes <asghiah@gmail.com>
2018-07-12 10:19:53 -0400

    Ash deal with headsign logging (#131)
    * move the headsign determination into the content message
    
    * compiler warnings
    
    * add tests for new logging messages
    
    * alewife hack
    
    * add the all northbound check back in
    
    * remove hack

--- lib/content/message/predictions.ex
+++ lib/content/message/predictions.ex
@@ -10,6 +10,8 @@ defmodule Content.Message.Predictions do
   yourself.
   """
 
+  require Logger
+
   @thirty_one_minutes 31 * 60
 
   @enforce_keys [:headsign, :minutes]
@@ -22,12 +24,20 @@ defmodule Content.Message.Predictions do
   }
 
   @spec non_terminal(Predictions.Prediction.t(), boolean) :: t()
-  def non_terminal(%Predictions.Prediction{headsign: headsign} = prediction, width \\ 18, stopped_at?) do
+  def non_terminal(prediction, width \\ 18, stopped_at?) do
     minutes = cond do
       stopped_at? -> :boarding
       prediction.seconds_until_arrival <= 30 -> :arriving
       prediction.seconds_until_arrival >= @thirty_one_minutes -> :thirty_plus
       true -> prediction.seconds_until_arrival |> Kernel./(60) |> round()
+    end
+
+    headsign = case headsign_for_prediction(prediction.route_id, prediction.direction_id, prediction.destination_stop_id) do
+      {:ok, dest} ->
+        dest
+      {:error, _} ->
+        Logger.warn "Could not find headsign for prediction #{inspect prediction}"
+        ""
     end
 
     %__MODULE__{
@@ -38,12 +48,20 @@ defmodule Content.Message.Predictions do
   end
 
   @spec terminal(Predictions.Prediction.t(), boolean()) :: t()
-  def terminal(%Predictions.Prediction{headsign: headsign} = prediction, width \\ 18, stopped_at?) do
+  def terminal(prediction, width \\ 18, stopped_at?) do
     minutes = case prediction.seconds_until_departure do
       x when x <= 30 and stopped_at? -> :boarding
       x when x <= 30 -> 1
       x when x >= @thirty_one_minutes -> :thirty_plus
       x -> x |> Kernel./(60) |> round()
+    end
+
+    headsign = case headsign_for_prediction(prediction.route_id, prediction.direction_id, prediction.destination_stop_id) do
+      {:ok, dest} ->
+        dest
+      {:error, _} ->
+        Logger.warn "Could not find headsign for prediction #{inspect prediction}"
+        ""
     end
 
     %__MODULE__{
@@ -52,6 +70,26 @@ defmodule Content.Message.Predictions do
       width: width,
     }
   end
+
+  @spec headsign_for_prediction(String.t(), 0 | 1, String.t()) :: {:ok, String.t()} | {:error, :not_found}
+  defp headsign_for_prediction("Mattapan", 0, _), do: {:ok, "Mattapan"}
+  defp headsign_for_prediction("Mattapan", 1, _), do: {:ok, "Ashmont"}
+  defp headsign_for_prediction("Orange", 0, _), do: {:ok, "Frst Hills"}
+  defp headsign_for_prediction("Orange", 1, _), do: {:ok, "Oak Grove"}
+  defp headsign_for_prediction("Blue", 0, _), do: {:ok, "Bowdoin"}
+  defp headsign_for_prediction("Blue", 1, _), do: {:ok, "Wonderland"}
+  defp headsign_for_prediction("Red", 1, _), do: {:ok, "Alewife"}
+  defp headsign_for_prediction("Red", 0, last_stop_id) when last_stop_id in ["70087", "70089", "70091", "70093"], do: {:ok, "Ashmont"}
+  defp headsign_for_prediction("Red", 0, last_stop_id) when last_stop_id in ["70097", "70101", "70103", "70105"], do: {:ok, "Braintree"}
+  defp headsign_for_prediction("Green-B", 0, _), do: {:ok, "Boston Col"}
+  defp headsign_for_prediction("Green-C", 0, _), do: {:ok, "Clvlnd Cir"}
+  defp headsign_for_prediction("Green-D", 0, _), do: {:ok, "Riverside"}
+  defp headsign_for_prediction("Green-E", 0, _), do: {:ok, "Heath St"}
+  defp headsign_for_prediction(_, 1, "70209"), do: {:ok, "Lechmere"}
+  defp headsign_for_prediction(_, 1, "70205"), do: {:ok, "North Sta"}
+  defp headsign_for_prediction(_, 1, "70201"), do: {:ok, "Govt Ctr"}
+  defp headsign_for_prediction(_, 1, "70200"), do: {:ok, "Park St"}
+  defp headsign_for_prediction(_, _, _), do: {:error, :not_found}
 
   defimpl Content.Message do
     require Logger
--- test/content/messages/predictions_test.exs
+++ test/content/messages/predictions_test.exs
@@ -1,11 +1,28 @@
 defmodule Content.Message.PredictionsTest do
   use ExUnit.Case, async: true
+  import ExUnit.CaptureLog
 
   describe "non_terminal/3" do
+    test "logs a warning when we cant find a headsign" do
+      prediction = %Predictions.Prediction{
+        seconds_until_arrival: 0,
+        direction_id: 1,
+        route_id: "NON-ROUTE",
+        destination_stop_id: "70261"
+      }
+      log = capture_log [level: :warn], fn ->
+        Content.Message.Predictions.non_terminal(prediction, false)
+      end
+
+      assert log =~ "Could not find headsign for prediction"
+    end
+
     test "puts ARR on the sign when train is 0 seconds away, but not boarding" do
       prediction = %Predictions.Prediction{
         seconds_until_arrival: 0,
-        headsign: "Ashmont"
+        direction_id: 1,
+        route_id: "Mattapan",
+        destination_stop_id: "70261"
       }
       msg = Content.Message.Predictions.non_terminal(prediction, false)
 
@@ -15,7 +32,9 @@ defmodule Content.Message.PredictionsTest do
     test "puts BRD on the sign when train is currently boarding" do
       prediction = %Predictions.Prediction{
         seconds_until_arrival: 0,
-        headsign: "Ashmont"
+        direction_id: 1,
+        route_id: "Mattapan",
+        destination_stop_id: "70261"
       }
       msg = Content.Message.Predictions.non_terminal(prediction, true)
 
@@ -25,7 +44,9 @@ defmodule Content.Message.PredictionsTest do
     test "puts arriving on the sign when train is 0-30 seconds away" do
       prediction = %Predictions.Prediction{
         seconds_until_arrival: 30,
-        headsign: "Mattapan"
+        direction_id: 0,
+        route_id: "Mattapan",
+        destination_stop_id: "70275"
       }
       msg = Content.Message.Predictions.non_terminal(prediction, false)
 
@@ -35,7 +56,9 @@ defmodule Content.Message.PredictionsTest do
     test "puts minutes on the sign when train is 31 seconds away" do
       prediction = %Predictions.Prediction{
         seconds_until_arrival: 31,
-        headsign: "Mattapan"
+        direction_id: 0,
+        route_id: "Mattapan",
+        destination_stop_id: "70275"
       }
       msg = Content.Message.Predictions.non_terminal(prediction, false)
 
@@ -45,7 +68,9 @@ defmodule Content.Message.PredictionsTest do
     test "Says 30+ min when train is more than 30 minutes away" do
       prediction = %Predictions.Prediction{
         seconds_until_arrival: 45 * 60,
-        headsign: "Mattapan"
+        direction_id: 0,
+        route_id: "Mattapan",
+        destination_stop_id: "70275"
       }
       msg = Content.Message.Predictions.non_terminal(prediction, false)
 
@@ -55,7 +80,9 @@ defmodule Content.Message.PredictionsTest do
     test "Says 30 min when train is exactly 30 minutes away" do
       prediction = %Predictions.Prediction{
         seconds_until_arrival: 30 * 60,
-        headsign: "Mattapan"
+        direction_id: 0,
+        route_id: "Mattapan",
+        destination_stop_id: "70275"
       }
       msg = Content.Message.Predictions.non_terminal(prediction, false)
 
@@ -63,7 +90,8 @@ defmodule Content.Message.PredictionsTest do
     end
 
     test "can use a shorter line length" do
-      prediction = %Predictions.Prediction{seconds_until_arrival: 550, headsign: "Mattapan"}
+      prediction = %Predictions.Prediction{seconds_until_arrival: 550, route_id: "Mattapan", direction_id: 0, destination_stop_id: "70275"}
+
       msg = Content.Message.Predictions.non_terminal(prediction, 15, false)
       assert Content.Message.to_string(msg) == "Mattapan  9 min"
     end
@@ -71,7 +99,9 @@ defmodule Content.Message.PredictionsTest do
     test "1 minute (singular) prediction" do
       prediction = %Predictions.Prediction{
         seconds_until_arrival: 65,
-        headsign: "Ashmont"
+        direction_id: 1,
+        route_id: "Mattapan",
+        destination_stop_id: "70261"
       }
       msg = Content.Message.Predictions.non_terminal(prediction, false)
 
@@ -81,36 +111,31 @@ defmodule Content.Message.PredictionsTest do
     test "multiple minutes prediction" do
       prediction = %Predictions.Prediction{
         seconds_until_arrival: 125,
-        headsign: "Ashmont"
+        direction_id: 1,
+        route_id: "Mattapan",
+        destination_stop_id: "70261"
       }
       msg = Content.Message.Predictions.non_terminal(prediction, false)
 
       assert Content.Message.to_string(msg) == "Ashmont      2 min"
     end
 
-    test "truncates very long headsigns to fit" do
-      prediction = %Predictions.Prediction{seconds_until_arrival: 125, headsign: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"}
-      msg = Content.Message.Predictions.non_terminal(prediction, false)
-
-      assert Content.Message.to_string(msg) == "ABCDEFGHIJK  2 min"
-    end
-
     test "Still shows predictions for negative arrivals" do
-      prediction = %Predictions.Prediction{seconds_until_arrival: -5, headsign: "abc"}
+      prediction = %Predictions.Prediction{seconds_until_arrival: -5, route_id: "Mattapan", direction_id: 1, destination_stop_id: "70261"}
       msg = Content.Message.Predictions.non_terminal(prediction, false)
 
-      assert Content.Message.to_string(msg) == "abc            ARR"
+      assert Content.Message.to_string(msg) == "Ashmont        ARR"
     end
 
     test "Shows BRD for negative arrival times if vehicle is STOPPED_AT" do
-      prediction = %Predictions.Prediction{seconds_until_arrival: -5, headsign: "abc"}
+      prediction = %Predictions.Prediction{seconds_until_arrival: -5, route_id: "Mattapan", direction_id: 1, destination_stop_id: "70261"}
       msg = Content.Message.Predictions.non_terminal(prediction, true)
 
-      assert Content.Message.to_string(msg) == "abc            BRD"
+      assert Content.Message.to_string(msg) == "Ashmont        BRD"
     end
 
     test "Rounds to the nearest minute" do
-      prediction = %Predictions.Prediction{seconds_until_arrival: 91, headsign: "Ashmont"}
+      prediction = %Predictions.Prediction{seconds_until_arrival: 91, route_id: "Mattapan", direction_id: 1, destination_stop_id: "70261"}
       msg = Content.Message.Predictions.non_terminal(prediction, false)
 
       assert Content.Message.to_string(msg) == "Ashmont      2 min"
@@ -118,10 +143,26 @@ defmodule Content.Message.PredictionsTest do
   end
 
   describe "terminal/3" do
+    test "logs a warning when we cant find a headsign" do
+      prediction = %Predictions.Prediction{
+        seconds_until_arrival: 0,
+        direction_id: 1,
+        route_id: "NON-ROUTE",
+        destination_stop_id: "70261"
+      }
+      log = capture_log [level: :warn], fn ->
+        Content.Message.Predictions.terminal(prediction, false)
+      end
+
+      assert log =~ "Could not find headsign for prediction"
+    end
+
     test "puts boarding on the sign when train is on the platform and predicted to depart in less than 30 seconds" do
       prediction = %Predictions.Prediction{
         seconds_until_departure: 0,
-        headsign: "Ashmont"
+        direction_id: 1,
+        route_id: "Mattapan",
+        destination_stop_id: "70261"
       }
       msg = Content.Message.Predictions.terminal(prediction, true)
 
@@ -131,7 +172,9 @@ defmodule Content.Message.PredictionsTest do
     test "does not put boarding if prediction is greater than 30 seconds" do
       prediction = %Predictions.Prediction{
         seconds_until_departure: 45,
-        headsign: "Ashmont"
+        direction_id: 1,
+        route_id: "Mattapan",
+        destination_stop_id: "70261"
       }
       msg = Content.Message.Predictions.terminal(prediction, true)
 
@@ -141,7 +184,9 @@ defmodule Content.Message.PredictionsTest do
     test "puts 1 min on the sign when train is not boarding, but is predicted to depart in less than a minute" do
       prediction = %Predictions.Prediction{
         seconds_until_departure: 10,
-        headsign: "Ashmont"
+        direction_id: 1,
+        route_id: "Mattapan",
+        destination_stop_id: "70261"
       }
       msg = Content.Message.Predictions.terminal(prediction, false)
 
@@ -151,7 +196,9 @@ defmodule Content.Message.PredictionsTest do
     test "puts the time on the sign when train's departure is more than 30 seconds away" do
       prediction = %Predictions.Prediction{
         seconds_until_departure: 60,
-        headsign: "Mattapan"
+        direction_id: 0,
+        route_id: "Mattapan",
+        destination_stop_id: "70275"
       }
       msg = Content.Message.Predictions.terminal(prediction, false)
 
