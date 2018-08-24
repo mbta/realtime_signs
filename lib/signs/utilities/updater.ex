d1de1274967a716de005b9614bf56e5fc4c83009
Dave Maltzan <dmaltzan@users.noreply.github.com>
2018-08-24 15:30:45 -0400

    Add logging of when stopped-train feature is turned on/off (#151)
    * Add logging of when stopped-train feature is turned on/off
    
    Since the current time isn't present in the code around this area, we'll have to get the time from the Splunk logs. I hope that's not too difficult.
    
    * format
    
    * spaces instead of commas
    
    * more cases to handle stops_away=0
    
    * oops
    
    * fix stupid mistake
    
    * test coverage
    
    * fix up test
    
    * format

--- lib/signs/utilities/updater.ex
+++ lib/signs/utilities/updater.ex
@@ -7,6 +7,7 @@ defmodule Signs.Utilities.Updater do
   """
 
   alias Signs.Utilities.SourceConfig
+  require Logger
 
   def update_sign(sign, {_top_src, top_msg} = top, {_bottom_src, bottom_msg} = bottom) do
     case {sign.current_content_top == top, sign.current_content_bottom == bottom} do
@@ -15,6 +16,8 @@ defmodule Signs.Utilities.Updater do
 
       # update top
       {false, true} ->
+        log_line_update(sign, top_msg, "top")
+
         sign.sign_updater.update_single_line(
           sign.pa_ess_id,
           "1",
@@ -29,6 +32,8 @@ defmodule Signs.Utilities.Updater do
 
       # update bottom
       {true, false} ->
+        log_line_update(sign, bottom_msg, "bottom")
+
         sign.sign_updater.update_single_line(
           sign.pa_ess_id,
           "2",
@@ -50,6 +55,9 @@ defmodule Signs.Utilities.Updater do
 
       # update both
       {false, false} ->
+        log_line_update(sign, top_msg, "top")
+        log_line_update(sign, bottom_msg, "bottom")
+
         sign.sign_updater.update_sign(
           sign.pa_ess_id,
           top_msg,
@@ -78,6 +86,72 @@ defmodule Signs.Utilities.Updater do
             tick_top: sign.expiration_seconds,
             tick_bottom: sign.expiration_seconds
         }
+    end
+  end
+
+  defp log_line_update(sign, msg, "top" = line) do
+    case {sign, msg} do
+      {%Signs.Realtime{id: sign_id, current_content_top: {_, %Content.Message.Predictions{}}},
+       %Content.Message.StoppedTrain{stops_away: msg_stops_away}}
+      when msg_stops_away > 0 ->
+        Logger.info("sign_id=#{sign_id} line=#{line} status=on")
+
+      {%Signs.Realtime{
+         id: sign_id,
+         current_content_top: {_, %Content.Message.StoppedTrain{stops_away: sign_stops_away}}
+       }, %Content.Message.StoppedTrain{stops_away: msg_stops_away}}
+      when sign_stops_away == 0 and msg_stops_away > 0 ->
+        Logger.info("sign_id=#{sign_id} line=#{line} status=on")
+
+      {%Signs.Realtime{
+         id: sign_id,
+         current_content_top: {_, %Content.Message.StoppedTrain{stops_away: sign_stops_away}}
+       }, %Content.Message.Predictions{}}
+      when sign_stops_away > 0 ->
+        Logger.info("sign_id=#{sign_id} line=#{line} status=off")
+
+      {%Signs.Realtime{
+         id: sign_id,
+         current_content_top: {_, %Content.Message.StoppedTrain{stops_away: sign_stops_away}}
+       }, %Content.Message.StoppedTrain{stops_away: msg_stops_away}}
+      when sign_stops_away > 0 and msg_stops_away == 0 ->
+        Logger.info("sign_id=#{sign_id} line=#{line} status=off")
+
+      _ ->
+        :ok
+    end
+  end
+
+  defp log_line_update(sign, msg, "bottom" = line) do
+    case {sign, msg} do
+      {%Signs.Realtime{id: sign_id, current_content_bottom: {_, %Content.Message.Predictions{}}},
+       %Content.Message.StoppedTrain{stops_away: msg_stops_away}}
+      when msg_stops_away > 0 ->
+        Logger.info("sign_id=#{sign_id} line=#{line} status=on")
+
+      {%Signs.Realtime{
+         id: sign_id,
+         current_content_bottom: {_, %Content.Message.StoppedTrain{stops_away: sign_stops_away}}
+       }, %Content.Message.StoppedTrain{stops_away: msg_stops_away}}
+      when sign_stops_away == 0 and msg_stops_away > 0 ->
+        Logger.info("sign_id=#{sign_id} line=#{line} status=on")
+
+      {%Signs.Realtime{
+         id: sign_id,
+         current_content_bottom: {_, %Content.Message.StoppedTrain{stops_away: sign_stops_away}}
+       }, %Content.Message.Predictions{}}
+      when sign_stops_away > 0 ->
+        Logger.info("sign_id=#{sign_id} line=#{line} status=off")
+
+      {%Signs.Realtime{
+         id: sign_id,
+         current_content_bottom: {_, %Content.Message.StoppedTrain{stops_away: sign_stops_away}}
+       }, %Content.Message.StoppedTrain{stops_away: msg_stops_away}}
+      when sign_stops_away > 0 and msg_stops_away == 0 ->
+        Logger.info("sign_id=#{sign_id} line=#{line} status=off")
+
+      _ ->
+        :ok
     end
   end
 
