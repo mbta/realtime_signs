c12cbb3d1edcb1528b371bc54f65445013226603
Alex Sghia-Hughes <asghiah@gmail.com>
2018-09-05 16:55:43 -0400

    minimum padding length (#160)

--- lib/content/utilities.ex
+++ lib/content/utilities.ex
@@ -1,6 +1,6 @@
 defmodule Content.Utilities do
   def width_padded_string(left, right, width) do
-    max_left_length = width - (String.length(right) + 2)
+    max_left_length = width - (String.length(right) + 1)
     left = String.slice(left, 0, max_left_length)
     padding = width - (String.length(left) + String.length(right))
     Enum.join([left, String.duplicate(" ", padding), right])
