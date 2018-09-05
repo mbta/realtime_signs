248885096a7f8799a635e627814723a390c6ae28
Alex Sghia-Hughes <alex@mbtace.com>
2018-09-05 16:33:28 -0400

    minimum padding length

--- test/content/utilities_test.exs
+++ test/content/utilities_test.exs
@@ -7,5 +7,9 @@ defmodule Content.UtilitiesTest do
     test "inserts spaces between left and right to reach given width" do
       assert width_padded_string("L", "R", 4) == "L  R"
     end
+
+    test "enforces a minimum spacing between the two strings of 1" do
+      assert width_padded_string("Left", "Right", 9) == "Lef Right"
+    end
   end
 end
