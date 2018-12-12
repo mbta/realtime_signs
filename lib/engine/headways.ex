47189218e61a2dee9a077186cd0c1b5353f0ef7e
Eddie Maldonado <emaldonado@mbta.com>
2018-12-12 12:17:00 -0500

    Calculate headway information every 5 minutes

--- lib/engine/headways.ex
+++ lib/engine/headways.ex
@@ -14,8 +14,10 @@ defmodule Engine.Headways do
 
   @type state :: %{
           ets_table_name: term(),
+          schedule_data: map(),
           fetcher: module(),
           fetch_ms: integer(),
+          headway_calc_ms: integer(),
           stop_ids: [String.t()]
         }
 
@@ -29,23 +31,27 @@ defmodule Engine.Headways do
 
   @spec init(Keyword.t()) :: {:ok, state()}
   def init(opts) do
-    fetch_ms = opts[:fetch_ms] || 60 * 60 * 1_000
-    fetcher = opts[:fetcher] || Application.get_env(:realtime_signs, :headway_requester)
     ets_table_name = opts[:ets_table_name] || __MODULE__
+    fetcher = opts[:fetcher] || Application.get_env(:realtime_signs, :headway_requester)
+    fetch_ms = opts[:fetch_ms] || 60 * 60 * 1_000
+    headway_calc_ms = opts[:headway_calc_ms] || 5 * 60 * 1_000
 
     ^ets_table_name =
       :ets.new(ets_table_name, [:set, :protected, :named_table, read_concurrency: true])
 
     state = %{
       ets_table_name: ets_table_name,
+      schedule_data: %{},
       fetcher: fetcher,
       fetch_ms: fetch_ms,
+      headway_calc_ms: headway_calc_ms,
       stop_ids: opts[:stop_ids]
     }
 
     :ets.insert(ets_table_name, Enum.map(state.stop_ids, fn x -> {x, :none} end))
 
-    schedule_update(self(), state)
+    send(self(), :update_hourly)
+    send(self(), :headway_update)
 
     {:ok, state}
   end
@@ -61,7 +67,13 @@ defmodule Engine.Headways do
   def handle_info(:update_hourly, state) do
     schedule_update(self(), state)
 
-    update(state)
+    update_schedule_data(state)
+  end
+
+  def handle_info(:headway_update, state) do
+    headway_update(self(), state)
+
+    update_headway_data(state)
   end
 
   def handle_info(msg, state) do
@@ -74,22 +86,39 @@ defmodule Engine.Headways do
     Process.send_after(pid, :update_hourly, state.fetch_ms)
   end
 
-  @spec update(state()) :: {:noreply, state()}
-  defp update(state) do
-    headway_updater = state[:fetcher]
-    headway_calculator = Application.get_env(:realtime_signs, :headway_calculator)
+  @spec headway_update(pid(), state()) :: reference()
+  defp headway_update(pid, state) do
+    Process.send_after(pid, :headway_update, state.headway_calc_ms)
+  end
 
-    schedules =
+  @spec update_schedule_data(state()) :: {:noreply, state()}
+  defp update_schedule_data(state) do
+    schedule_updater = state[:fetcher]
+
+    Map.put(
+      state,
+      :schedule_data,
       state.stop_ids
-      |> headway_updater.get_schedules()
+      |> schedule_updater.get_schedules()
       |> Enum.group_by(fn schedule ->
         schedule["relationships"]["stop"]["data"]["id"]
       end)
+    )
 
-    # TODO: We should figure out what to do about the current time issue
+    {:noreply, state}
+  end
+
+  @spec update_headway_data(state()) :: {:noreply, state()}
+  defp update_headway_data(state) do
+    headway_calculator = Application.get_env(:realtime_signs, :headway_calculator)
+
     headways =
       Enum.map(
-        headway_calculator.group_headways_for_stations(schedules, state.stop_ids, Timex.now()),
+        headway_calculator.group_headways_for_stations(
+          state.schedule_data,
+          state.stop_ids,
+          Timex.now()
+        ),
         fn {k, v} -> {k, v} end
       )
 
