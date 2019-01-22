d39a4306bcfa310474f498404faa4b98f7a95901
Alex Sghia-Hughes <alex@mbtace.com>
2019-01-22 11:49:30 -0500

    handle empty lines

--- lib/content/audio/custom.ex
+++ lib/content/audio/custom.ex
@@ -21,6 +21,21 @@ defmodule Content.Audio.Custom do
     }
   end
 
+  def from_messages(%Content.Message.Custom{line: :top, message: audio}, %Content.Message.Empty{}) do
+    %__MODULE__{
+      message: audio
+    }
+  end
+
+  def from_messages(%Content.Message.Empty{}, %Content.Message.Custom{
+        line: :bottom,
+        message: audio
+      }) do
+    %__MODULE__{
+      message: audio
+    }
+  end
+
   def from_messages(_top, _bottom) do
     nil
   end
