fb9f014ba5b2e0bc74dee8951ff8c3312e399c7f
Alex Sghia-Hughes <asghiah@gmail.com>
2019-01-23 14:37:41 -0500

    Ash shuttle endpoints (#219)
    * add some logging for debugging
    
    * log less
    
    * remove logger
    
    * handle middle of the suspension stations
    
    * handle suspensions the same way

--- lib/signs/utilities/messages.ex
+++ lib/signs/utilities/messages.ex
@@ -23,20 +23,24 @@ defmodule Signs.Utilities.Messages do
       !enabled? ->
         {{nil, Content.Message.Empty.new()}, {nil, Content.Message.Empty.new()}}
 
-      alert_status == :shuttles_transfer_station ->
-        {{nil, Content.Message.Empty.new()}, {nil, Content.Message.Empty.new()}}
-
-      alert_status == :shuttles_closed_station ->
-        {{nil, %Content.Message.Alert.NoService{mode: :train}},
-         {nil, %Content.Message.Alert.UseShuttleBus{}}}
-
-      alert_status == :suspension ->
-        {{nil, %Content.Message.Alert.NoService{mode: :none}}, {nil, Content.Message.Empty.new()}}
-
       true ->
         case Signs.Utilities.Predictions.get_messages(sign) do
           {{nil, %Content.Message.Empty{}}, {nil, %Content.Message.Empty{}}} ->
-            Signs.Utilities.Headways.get_messages(sign)
+            case alert_status do
+              :shuttles_transfer_station ->
+                {{nil, Content.Message.Empty.new()}, {nil, Content.Message.Empty.new()}}
+
+              :shuttles_closed_station ->
+                {{nil, %Content.Message.Alert.NoService{mode: :train}},
+                 {nil, %Content.Message.Alert.UseShuttleBus{}}}
+
+              :suspension ->
+                {{nil, %Content.Message.Alert.NoService{mode: :none}},
+                 {nil, Content.Message.Empty.new()}}
+
+              _ ->
+                Signs.Utilities.Headways.get_messages(sign)
+            end
 
           messages ->
             messages
