b9d34809f2d9590f604817a6718dc4c2288d67b8
Gabe Durazo <gabe@mbtace.com>
2019-02-28 14:07:28 -0500

    Remove unused inflex dependency

--- mix.exs
+++ mix.exs
@@ -46,8 +46,7 @@ defmodule RealtimeSigns.Mixfile do
       {:logger_splunk_backend, git: "https://github.com/mbta/logger_splunk_backend.git"},
       {:poison, "~> 3.1"},
       {:sentry, "~> 6.2"},
-      {:timex, "~> 3.1.0"},
-      {:inflex, "~> 1.8.1"}
+      {:timex, "~> 3.1.0"}
     ]
   end
 end
