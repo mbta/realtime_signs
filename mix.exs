eff8299788a990aaa5d55d63bc432f3ff47d86c1
Gabe Durazo <gabe@mbtace.com>
2019-02-28 14:07:01 -0500

    Update sentry dependency

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule RealtimeSigns.Mixfile do
       {:httpoison, "~> 1.0"},
       {:logger_splunk_backend, git: "https://github.com/mbta/logger_splunk_backend.git"},
       {:poison, "~> 3.1"},
-      {:sentry, "~> 6.2.1"},
+      {:sentry, "~> 6.2"},
       {:timex, "~> 3.1.0"},
       {:inflex, "~> 1.8.1"}
     ]
