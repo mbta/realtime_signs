bf149e8adebae9a31e723470146e112ae50c1551
Eddie Maldonado <eddie@lemald.org>
2019-03-13 17:31:37 -0400

    Give audio-visual track info at terminals (#254)
    * Include track information in audio
    
    * Fix test; refine to_params logic
    
    * Test message-level
    
    * Move all track logic into audio layer
    
    * Test for Content.Audio.Predictions
    
    * Move logic to allow for track numbers on countdown messages
    
    * Implement functionality for countdown messages
    
    * Fix text based on feedback from Andrew
    
    * Rename @in_word to @in_

--- lib/content/audio/next_train_countdown.ex
+++ lib/content/audio/next_train_countdown.ex
@@ -3,7 +3,7 @@ defmodule Content.Audio.NextTrainCountdown do
   The next train to [destination] arrives in [n] minutes.
   """
 
-  @enforce_keys [:destination, :verb, :minutes]
+  @enforce_keys [:destination, :verb, :minutes, :stop_id]
   defstruct @enforce_keys ++ [platform: nil]
 
   @type verb :: :arrives | :departs
@@ -13,6 +13,7 @@ defmodule Content.Audio.NextTrainCountdown do
           destination: PaEss.terminal_station(),
           verb: verb(),
           minutes: integer(),
+          stop_id: String.t(),
           platform: :ashmont | :braintree | nil
         }
 
@@ -21,13 +22,34 @@ defmodule Content.Audio.NextTrainCountdown do
   defimpl Content.Audio do
     alias PaEss.Utilities
 
+    @the_next "501"
+    @train_to "507"
+    @on_track_1 "541"
+    @on_track_2 "542"
+    @in_ "504"
+    @minutes "505"
+    @space "21000"
+
     def to_params(%{platform: nil, minutes: 1} = audio) do
-      {"141", [Utilities.destination_var(audio.destination), verb_var(audio)], :audio}
+      case Content.Utilities.stop_track_number(audio.stop_id) do
+        nil ->
+          {"141", [Utilities.destination_var(audio.destination), verb_var(audio)], :audio}
+
+        track_number ->
+          terminal_track_params(audio, track_number)
+      end
     end
 
     def to_params(%{platform: nil} = audio) do
-      {"90", [Utilities.destination_var(audio.destination), verb_var(audio), minutes_var(audio)],
-       :audio}
+      case Content.Utilities.stop_track_number(audio.stop_id) do
+        nil ->
+          {"90",
+           [Utilities.destination_var(audio.destination), verb_var(audio), minutes_var(audio)],
+           :audio}
+
+        track_number ->
+          terminal_track_params(audio, track_number)
+      end
     end
 
     def to_params(%{minutes: 1} = audio) do
@@ -49,6 +71,32 @@ defmodule Content.Audio.NextTrainCountdown do
        ], :audio}
     end
 
+    @spec terminal_track_params(
+            Content.Audio.NextTrainCountdown.t(),
+            Content.Utilities.track_number()
+          ) :: {String.t(), [String.t()], :audio_visual}
+    defp terminal_track_params(audio, track_number) do
+      vars = [
+        @the_next,
+        @space,
+        @train_to,
+        @space,
+        Utilities.destination_var(audio.destination),
+        @space,
+        verb_var(audio),
+        @space,
+        @in_,
+        @space,
+        minutes_var(audio),
+        @space,
+        @minutes,
+        @space,
+        track(track_number)
+      ]
+
+      {Utilities.take_message_id(vars), vars, :audio_visual}
+    end
+
     defp platform_var(%{platform: :ashmont}), do: "4016"
     defp platform_var(%{platform: :braintree}), do: "4021"
 
@@ -58,5 +106,9 @@ defmodule Content.Audio.NextTrainCountdown do
     defp minutes_var(%{minutes: minutes}) do
       Utilities.countdown_minutes_var(minutes)
     end
+
+    @spec track(Content.Utilities.track_number()) :: String.t()
+    defp track(1), do: @on_track_1
+    defp track(2), do: @on_track_2
   end
 end
