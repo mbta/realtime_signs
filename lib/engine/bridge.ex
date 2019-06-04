1d745a0529c6557b9a6997098b2f69e1414c9e5d
Maldonado <emaldonado@mbta.com>
2019-06-04 15:33:02 -0400

    Updates to bridge engine
    * Fetch every minute instead of every 10 minutes
    * Store state in an ETS table

--- lib/engine/bridge.ex
+++ lib/engine/bridge.ex
@@ -10,34 +10,51 @@ defmodule Engine.Bridge do
   @type status :: String.t() | nil
   @type duration :: integer | nil
 
-  @type t :: %{bridge_id => {status, duration}}
+  @type state :: %{
+          ets_table_name: term()
+        }
+
+  @fetch_ms 60 * 1_000
 
   def start_link(name \\ __MODULE__) do
     GenServer.start_link(__MODULE__, [], name: name)
   end
 
-  @spec status(GenServer.server(), bridge_id) :: {status, duration}
-  def status(pid \\ __MODULE__, id) do
-    GenServer.call(pid, {:status, id})
+  @spec status(term(), bridge_id) :: {status, duration} | nil
+  def status(ets_table_name \\ __MODULE__, id) do
+    case :ets.lookup(ets_table_name, id) do
+      [{^id, status}] -> status
+      _ -> nil
+    end
   end
 
-  @spec update(GenServer.server()) :: t
+  @spec update(GenServer.server()) :: any()
   def update(pid \\ __MODULE__) do
     Kernel.send(pid, :update)
   end
 
-  @spec init(any()) :: {:ok, any()}
-  def init(_) do
+  @spec init(Keyword.t()) :: {:ok, any()}
+  def init(opts) do
+    ets_table_name = opts[:ets_table_name] || __MODULE__
+
+    ^ets_table_name =
+      :ets.new(ets_table_name, [:set, :protected, :named_table, read_concurrency: true])
+
     schedule_update(self())
-    {:ok, %{}}
+
+    state = %{ets_table_name: ets_table_name}
+
+    {:ok, state}
   end
 
-  def handle_info(:update, _state) do
+  def handle_info(:update, state) do
     schedule_update(self())
     bridge_request = Application.get_env(:realtime_signs, :bridge_requester)
     bridge_status = bridge_request.get_status("1", Timex.now())
 
-    {:noreply, %{"1" => bridge_status}}
+    :ets.insert(state.ets_table_name, [{"1", bridge_status}])
+
+    {:noreply, state}
   end
 
   def handle_info(msg, state) do
@@ -45,11 +62,7 @@ defmodule Engine.Bridge do
     {:noreply, state}
   end
 
-  def handle_call({:status, id}, _from, state) do
-    {:reply, state[id], state}
-  end
-
   defp schedule_update(pid) do
-    Process.send_after(pid, :update, 60 * 10_000)
+    Process.send_after(pid, :update, @fetch_ms)
   end
 end
