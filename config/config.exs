b0cf3f30f3890db5e98f2b903b83b678562bfd67
Maldonado <emaldonado@mbta.com>
2019-06-20 09:56:52 -0400

    Revert "Don't show headway for RL signs (#278)"
    This reverts commit 250258aeb61f45f924017f7f1c3d4ded3a35e054.

--- config/config.exs
+++ config/config.exs
@@ -48,8 +48,7 @@ config :realtime_signs,
   s3_path: System.get_env("SIGNS_S3_PATH"),
   api_v3_key: System.get_env("API_V3_KEY"),
   api_v3_url: System.get_env("API_V3_URL") || "https://green.dev.api.mbtace.com",
-  number_of_http_updaters: String.to_integer(System.get_env("NUMBER_OF_HTTP_UPDATERS") || "4"),
-  no_headway_on_rl: System.get_env("NO_HEADWAY_ON_RL") || false
+  number_of_http_updaters: String.to_integer(System.get_env("NUMBER_OF_HTTP_UPDATERS") || "4")
 
 config :ex_aws,
   access_key_id: [{:system, "SIGNS_S3_CONFIG_KEY"}, :instance_role],
