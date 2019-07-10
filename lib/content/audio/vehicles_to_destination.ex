890578900a14f197974508a2766e9b5eb56b5f3a
Alex Sghia-Hughes <alex@mbtace.com>
2019-07-10 11:23:04 -0400

    add a test for the headway direction names

--- lib/content/audio/vehicles_to_destination.ex
+++ lib/content/audio/vehicles_to_destination.ex
@@ -72,7 +72,7 @@ defmodule Content.Audio.VehiclesToDestination do
   defp convert_destination("Bowdoin"), do: {:ok, :bowdoin}
   defp convert_destination("Frst Hills"), do: {:ok, :forest_hills}
   defp convert_destination("Oak Grove"), do: {:ok, :oak_grove}
-  defp convert_destination("Park Sreet"), do: {:ok, :park_street}
+  defp convert_destination("Park Street"), do: {:ok, :park_street}
   defp convert_destination("Govt Ctr"), do: {:ok, :govt_ctr}
   defp convert_destination("North Station"), do: {:ok, :north_sta}
   defp convert_destination("Lechmere"), do: {:ok, :lechmere}
