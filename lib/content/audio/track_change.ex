d8ad40cea14792bd2d9e5918bca2c8bad303f122
Maldonado <emaldonado@mbta.com>
2019-11-08 16:01:55 -0500

    Add spaces to track change message

--- lib/content/audio/track_change.ex
+++ lib/content/audio/track_change.ex
@@ -21,17 +21,24 @@ defmodule Content.Audio.TrackChange do
     @is_now_boarding "544"
     @on_track_1 "541"
     @on_track_2 "542"
+    @space "21000"
 
     def to_params(audio) do
       case PaEss.Utilities.destination_var(audio.destination) do
         {:ok, dest_var} ->
           vars = [
             @track_change,
+            @space,
             @the_next,
+            @space,
             branch_letter(audio.route_id),
+            @space,
             @train_to,
+            @space,
             dest_var,
+            @space,
             @is_now_boarding,
+            @space,
             track(audio.track)
           ]
 
