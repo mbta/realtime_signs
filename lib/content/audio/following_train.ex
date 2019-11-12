89f50cdc3ec598460c8d74d4fba005831bd5090d
Maldonado <emaldonado@mbta.com>
2019-11-12 15:06:29 -0500

    Announce branch letters for next / following train

--- lib/content/audio/following_train.ex
+++ lib/content/audio/following_train.ex
@@ -57,6 +57,13 @@ defmodule Content.Audio.FollowingTrain do
   defimpl Content.Audio do
     alias PaEss.Utilities
 
+    @the_following "667"
+    @train_to "507"
+    @in_ "504"
+    @minutes "505"
+    @minute "532"
+    @space "21000"
+
     def to_params(%{destination: :southbound, verb: verb, minutes: minutes}) do
       min_or_mins = if minutes == 1, do: "minute", else: "minutes"
       text = "The following southbound train #{verb} in #{minutes} #{min_or_mins}"
@@ -66,10 +73,21 @@ defmodule Content.Audio.FollowingTrain do
     def to_params(audio) do
       case Utilities.destination_var(audio.destination) do
         {:ok, dest_var} ->
-          if audio.minutes == 1 do
-            {:canned, {"159", [dest_var, verb_var(audio)], :audio}}
-          else
-            {:canned, {"160", [dest_var, verb_var(audio), minutes_var(audio)], :audio}}
+          green_line_branch =
+            Content.Utilities.route_and_destination_branch_letter(
+              audio.route_id,
+              audio.destination
+            )
+
+          cond do
+            !is_nil(green_line_branch) ->
+              green_line_with_branch_params(audio, green_line_branch, dest_var)
+
+            audio.minutes == 1 ->
+              {:canned, {"159", [dest_var, verb_var(audio)], :audio}}
+
+            true ->
+              {:canned, {"160", [dest_var, verb_var(audio), minutes_var(audio)], :audio}}
           end
 
         {:error, :unknown} ->
@@ -81,11 +99,39 @@ defmodule Content.Audio.FollowingTrain do
       end
     end
 
+    @spec green_line_with_branch_params(
+            Content.Audio.FollowingTrain.t(),
+            Content.Utilities.green_line_branch(),
+            String.t()
+          ) :: Content.Audio.canned_message()
+    defp green_line_with_branch_params(audio, green_line_branch, destination_var) do
+      vars =
+        Enum.intersperse(
+          [
+            @the_following,
+            PaEss.Utilities.green_line_branch_var(green_line_branch),
+            @train_to,
+            destination_var,
+            verb_var(audio),
+            @in_,
+            minutes_var(audio),
+            minute_or_minutes(audio)
+          ],
+          @space
+        )
+
+      {:canned, {Utilities.take_message_id(vars), vars, :audio}}
+    end
+
     defp verb_var(%{verb: :arrives}), do: "503"
     defp verb_var(%{verb: :departs}), do: "502"
 
     defp minutes_var(%{minutes: minutes}) do
       Utilities.countdown_minutes_var(minutes)
     end
+
+    @spec minute_or_minutes(Content.Audio.FollowingTrain.t()) :: String.t()
+    defp minute_or_minutes(%Content.Audio.FollowingTrain{minutes: 1}), do: @minute
+    defp minute_or_minutes(%Content.Audio.FollowingTrain{}), do: @minutes
   end
 end
