d6c6a4037a3ac87cd4b0289ed8fe2358e7845e8f
Gabe Durazo <gdurazo@mbta.com>
2019-12-18 13:52:41 -0600

    fix: variable not picked up on Windows

--- build_release.sh
+++ build_release.sh
@@ -2,13 +2,11 @@
 
 # Run this from the repo root directory (e.g. /c/Users/RTRUser/GitHub/realtime_signs_release_dev)
 
-MIX_ENV=prod
 ERL_DIR="erl10.5"
 ELIXIR_DIR="elixir1.9.4"
-PATH="/c/Users/RTRUser/bin/$ERL_DIR/bin/:/c/Users/RTRUser/bin/$ELIXIR_DIR/bin/:$PATH"
 
 rm -r _build/
-mix deps.get
-mix release
+env MIX_ENV=prod PATH="/c/Users/RTRUser/bin/$ERL_DIR/bin/:/c/Users/RTRUser/bin/$ELIXIR_DIR/bin/:$PATH" mix deps.get
+env MIX_ENV=prod PATH="/c/Users/RTRUser/bin/$ERL_DIR/bin/:/c/Users/RTRUser/bin/$ELIXIR_DIR/bin/:$PATH" mix release
 
 echo "Release is built. Restart the Windows service to run it."
