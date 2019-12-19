719cb0ccc7ccefcbc96c97cbbfc069f3c79e4f76
Gabe Durazo <gdurazo@mbta.com>
2019-12-19 10:11:34 -0600

    feat: Different node names in release

--- build_release.sh
+++ build_release.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
+set -e
 
 # Run this from the repo root directory (e.g. /c/Users/RTRUser/GitHub/realtime_signs_release_dev)
+# Takes the release node name as an argument (e.g. `$ ./build_release.sh realtime_signs_dev`)
+
+if [ $# -eq 0 ]; then
+  echo "No argument provided"
+  exit 1
+fi
 
 ERL_DIR="erl10.5"
 ELIXIR_DIR="elixir1.9.4"
 
-rm -r _build/
+rm -rf _build/
 env MIX_ENV=prod PATH="/c/Users/RTRUser/bin/$ERL_DIR/bin/:/c/Users/RTRUser/bin/$ELIXIR_DIR/bin/:$PATH" mix deps.get
-env MIX_ENV=prod PATH="/c/Users/RTRUser/bin/$ERL_DIR/bin/:/c/Users/RTRUser/bin/$ELIXIR_DIR/bin/:$PATH" mix release
+env MIX_ENV=prod PATH="/c/Users/RTRUser/bin/$ERL_DIR/bin/:/c/Users/RTRUser/bin/$ELIXIR_DIR/bin/:$PATH" RELEASE_NODE=$1 mix release
 
 echo "Release is built. Restart the Windows service to run it."
