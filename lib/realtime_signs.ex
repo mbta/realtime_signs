c67692e7a855e5f2160e19907393fbe411bee2c6
Gabe Durazo <gdurazo@mbta.com>
2019-12-30 08:45:25 -0600

    refactor: Change RealtimeSignsConfig to accept keyword list for opts.

--- lib/realtime_signs.ex
+++ lib/realtime_signs.ex
@@ -30,18 +30,20 @@ defmodule RealtimeSigns do
   @spec runtime_config() :: :ok
   def runtime_config do
     env = System.get_env()
-    :ok = Config.update_env(env, :sign_head_end_host, "SIGN_HEAD_END_HOST", :string)
-    :ok = Config.update_env(env, :sign_ui_url, "SIGN_UI_URL", :string)
-    :ok = Config.update_env(env, :sign_ui_api_key, "SIGN_UI_API_KEY", :string)
-    :ok = Config.update_env(env, :bridge_api_username, "BRIDGE_API_USERNAME", :string)
-    :ok = Config.update_env(env, :bridge_api_password, "BRIDGE_API_PASSWORD", :string)
-    :ok = Config.update_env(env, :trip_update_url, "TRIP_UPDATE_URL", :string)
-    :ok = Config.update_env(env, :vehicle_positions_url, "VEHICLE_POSITIONS_URL", :string)
-    :ok = Config.update_env(env, :s3_bucket, "SIGNS_S3_BUCKET", :string)
-    :ok = Config.update_env(env, :s3_path, "SIGNS_S3_PATH", :string)
-    :ok = Config.update_env(env, :api_v3_key, "API_V3_KEY", :string)
-    :ok = Config.update_env(env, :api_v3_url, "API_V3_URL", :string)
-    :ok = Config.update_env(env, :number_of_http_updaters, "NUMBER_OF_HTTP_UPDATERS", :integer)
+    :ok = Config.update_env(env, :sign_head_end_host, "SIGN_HEAD_END_HOST")
+    :ok = Config.update_env(env, :sign_ui_url, "SIGN_UI_URL")
+    :ok = Config.update_env(env, :sign_ui_api_key, "SIGN_UI_API_KEY")
+    :ok = Config.update_env(env, :bridge_api_username, "BRIDGE_API_USERNAME")
+    :ok = Config.update_env(env, :bridge_api_password, "BRIDGE_API_PASSWORD")
+    :ok = Config.update_env(env, :trip_update_url, "TRIP_UPDATE_URL")
+    :ok = Config.update_env(env, :vehicle_positions_url, "VEHICLE_POSITIONS_URL")
+    :ok = Config.update_env(env, :s3_bucket, "SIGNS_S3_BUCKET")
+    :ok = Config.update_env(env, :s3_path, "SIGNS_S3_PATH")
+    :ok = Config.update_env(env, :api_v3_key, "API_V3_KEY")
+    :ok = Config.update_env(env, :api_v3_url, "API_V3_URL")
+
+    :ok =
+      Config.update_env(env, :number_of_http_updaters, "NUMBER_OF_HTTP_UPDATERS", type: :integer)
   end
 
   def http_updater_children do
--- lib/realtime_sigs_config.ex
+++ lib/realtime_sigs_config.ex
@@ -1,7 +1,9 @@
 defmodule RealtimeSignsConfig do
   @doc "Helper for setting configuration at runtime"
-  @spec update_env(map(), atom(), String.t(), :string | :integer) :: :ok | no_return()
-  def update_env(env, app_key, env_var, type) do
+  @spec update_env(map(), atom(), String.t(), Keyword.t()) :: :ok | no_return()
+  def update_env(env, app_key, env_var, opts \\ []) do
+    type = Keyword.get(opts, :type, :string)
+
     case Map.get(env, env_var) do
       nil ->
         :ok
