0c926da6b8f69cfe2d3e1b98dddca0b1001d548b
Gabe Durazo <gdurazo@mbta.com>
2019-12-30 08:45:25 -0600

    refactor: Change RealtimeSignsConfig to accept keyword list for opts.

--- test/realtime_signs_config_test.exs
+++ test/realtime_signs_config_test.exs
@@ -9,17 +9,17 @@ defmodule RealtimeSignsConfigTest do
 
   describe "update_env/5" do
     test "sets the application environment" do
-      assert :ok = update_env(%{"ENV_VAR" => "foo"}, :app_key, "ENV_VAR", :string)
+      assert :ok = update_env(%{"ENV_VAR" => "foo"}, :app_key, "ENV_VAR")
       assert Application.get_env(:realtime_signs, :app_key) == "foo"
     end
 
     test "still returns :ok if missing, and doesn't update app environment" do
-      assert :ok = update_env(%{}, :app_key, "ENV_VAR", :string)
+      assert :ok = update_env(%{}, :app_key, "ENV_VAR")
       assert Application.get_env(:realtime_signs, :app_key) == nil
     end
 
     test "converts an integer before storing in application environment" do
-      assert :ok = update_env(%{"ENV_VAR" => "5"}, :app_key, "ENV_VAR", :integer)
+      assert :ok = update_env(%{"ENV_VAR" => "5"}, :app_key, "ENV_VAR", type: :integer)
       assert Application.get_env(:realtime_signs, :app_key) == 5
     end
   end
