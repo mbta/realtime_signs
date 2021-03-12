825aee3685439417e1875fba1617731c46be8444
Gabe Durazo <gdurazo@mbta.com>
2021-03-12 10:31:55 -0600

    feat: Engine.Health checks network connectivity

--- test/support/helpers.ex
+++ test/support/helpers.ex
@@ -19,4 +19,21 @@ defmodule Test.Support.Helpers do
       end)
     end
   end
+
+  @doc "Starts an inets server and returns the port, for stubbing network requests"
+  @spec start_server(module()) :: {:ok, pid(), integer()}
+  def start_server(module) do
+    {:ok, pid} =
+      :inets.start(:httpd,
+        server_name: 'TmpServer',
+        server_root: '/tmp',
+        document_root: '/tmp',
+        port: 0,
+        modules: [module]
+      )
+
+    port = :httpd.info(pid) |> Keyword.get(:port)
+
+    {:ok, pid, port}
+  end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,4 @@
+Application.ensure_all_started(:inets)
 ExUnit.start()
+
+Mox.defmock(Engine.NetworkCheck.Mock, for: Engine.NetworkCheck)
