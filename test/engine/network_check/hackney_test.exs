46b31bb4d25d07519e1b99562fb47a854e58bcba
Gabe Durazo <gdurazo@mbta.com>
2021-04-26 07:47:01 -0500

    fix: Consider any HTTP status 200-299 as successful for health check

--- test/engine/network_check/hackney_test.exs
+++ test/engine/network_check/hackney_test.exs
@@ -6,13 +6,24 @@ defmodule Engine.NetworkCheck.HackneyTest do
     def unquote(:do)(_data), do: {:proceed, [response: {500, 'Not OK'}]}
   end
 
-  defmodule MockGoodNetwork do
+  defmodule MockGoodNetwork200 do
     def unquote(:do)(_data), do: {:proceed, [response: {200, 'OK'}]}
+  end
+
+  defmodule MockGoodNetwork204 do
+    def unquote(:do)(_data), do: {:proceed, [response: {204, 'OK'}]}
   end
 
   describe "check/0" do
     test "returns :ok on valid check" do
-      {:ok, pid, port} = Helpers.start_server(MockGoodNetwork)
+      {:ok, pid, port} = Helpers.start_server(MockGoodNetwork200)
+      on_exit(fn -> :inets.stop(:httpd, pid) end)
+
+      assert Engine.NetworkCheck.Hackney.check("http://localhost:#{port}/") == :ok
+    end
+
+    test "returns :ok on 204 response" do
+      {:ok, pid, port} = Helpers.start_server(MockGoodNetwork204)
       on_exit(fn -> :inets.stop(:httpd, pid) end)
 
       assert Engine.NetworkCheck.Hackney.check("http://localhost:#{port}/") == :ok
