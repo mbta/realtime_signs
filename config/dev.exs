031dc73455513da58a52a2d023c613a129c8ed52
Paul Kim <pkim@mbta.com>
2022-04-26 19:06:37 -0400

    add back splunk logger backend

--- config/dev.exs
+++ config/dev.exs
@@ -1,4 +1,11 @@
 use Mix.Config
 
 config :logger,
-  backends: [:console]
+  backends: [{Logger.Backend.Splunk, :splunk}, :console]
+
+config :logger, :splunk,
+  connector: Logger.Backend.Splunk.Output.Http,
+  host: 'https://http-inputs-mbta.splunkcloud.com/services/collector/event',
+  token: {:system, "SIGNS_SPLUNK_TOKEN"},
+  format: "$dateT$time [$level]$levelpad $metadata$message\n",
+  metadata: [:request_id]
