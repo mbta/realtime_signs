2fd22a013fa9b1402a2097742198ff391eb44d04
Paul Kim <pkim@mbta.com>
2022-04-29 18:15:01 -0400

    Remove splunk config from dev config

--- config/dev.exs
+++ config/dev.exs
@@ -6,10 +6,3 @@ config :realtime_signs, RealtimeSignsWeb.Endpoint,
 
 config :logger,
   backends: [:console]
-
-config :logger, :splunk,
-  connector: Logger.Backend.Splunk.Output.Http,
-  host: 'https://http-inputs-mbta.splunkcloud.com/services/collector/event',
-  token: {:system, "SIGNS_SPLUNK_TOKEN"},
-  format: "$dateT$time [$level]$levelpad $metadata$message\n",
-  metadata: [:request_id]
