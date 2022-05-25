a3126e04c3efd9c97ce8f60d30ae5855ca33dd12
Paul Kim <pkim@mbta.com>
2022-05-25 18:37:05 -0400

    add temporary logging

--- lib/realtime_signs_web/router.ex
+++ lib/realtime_signs_web/router.ex
@@ -1,10 +1,13 @@
 defmodule RealtimeSignsWeb.Router do
   use RealtimeSignsWeb, :router
 
+  require Logger
+
   pipeline :api do
     plug(:accepts, ["json"])
   end
 
+  Logger.info("monitor sign uptime: #{inspect(Application.get_env(:realtime_signs, :monitor_sign_scu_uptime))}")
   if (Application.get_env(:realtime_signs, :monitor_sign_scu_uptime) === "true") do
     scope "/monitoring", RealtimeSignsWeb do
       pipe_through([:api])
