f5e9173f199052d54719e5236347cbbf196175f4
Paul Kim <pkim@mbta.com>
2022-06-09 13:20:52 -0400

    Add some unit tests

---
+++ test/monitoring/uptime_test.ex
@@ -0,0 +1,27 @@
+defmodule Monitoring.UptimeTest do
+  use ExUnit.Case, async: true
+
+  test "Processes SCU uptime data" do
+    nodes = [
+      %{
+        "description" => "line:station:UNITTESTSCU001:SCU",
+        "node_type" => "PSS",
+        "is_online" => "true"
+      }
+    ]
+
+    assert :ok = Monitoring.Uptime.monitor_device_uptime(nodes, 1_234_567_890)
+  end
+
+  test "Processes sign uptime data" do
+    nodes = [
+      %{
+        "description" => "line:station:UNITTESTSIGN001:C",
+        "node_type" => "SGN",
+        "is_online" => "true"
+      }
+    ]
+
+    assert :ok = Monitoring.Uptime.monitor_device_uptime(nodes, 1_234_567_890)
+  end
+end
