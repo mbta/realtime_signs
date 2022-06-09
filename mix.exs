a06e97068231a363820886668129e4efe1a0a2c3
Paul Kim <pkim@mbta.com>
2022-06-09 16:25:51 -0400

    fix dialyzer

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule RealtimeSigns.Mixfile do
       elixirc_paths: elixirc_paths(Mix.env()),
       dialyzer: [
         plt_add_apps: [:mix, :hackney, :httpd, :inets],
-        plt_add_deps: true,
+        plt_add_deps: :app_tree,
         ignore_warnings: ".dialyzer.ignore-warnings"
       ],
       releases: [
