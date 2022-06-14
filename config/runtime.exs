786a42a8aa5cc337a448732caf707c3148ccddcf
Paul Kim <pkim@mbta.com>
2022-06-14 15:08:00 -0400

    move SECRET_KEY_BASE to runtime config

--- config/runtime.exs
+++ config/runtime.exs
@@ -4,3 +4,7 @@ config :realtime_signs, RealtimeSignsWeb.Endpoint,
   http: [port: System.get_env("MONITOR_SIGN_SCU_PORT")],
   url: [host: "localhost"],
   server: true
+
+if config_env() == :prod do
+  config :realtime_signs, RealtimeSignsWeb.Endpoint, secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
+end
