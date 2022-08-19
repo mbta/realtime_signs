a1ce69578a01a0e3f2210f691e7d5110fd412cc2
Paul Kim <pkim@mbta.com>
2022-08-19 12:37:38 -0400

    add error view

--- lib/realtime_signs_web.ex
+++ lib/realtime_signs_web.ex
@@ -7,6 +7,20 @@ defmodule RealtimeSignsWeb do
     end
   end
 
+  def view do
+    quote do
+      use Phoenix.View,
+        root: "lib/realtime_signs_web/templates",
+        namespace: RealtimeSignsWeb
+
+      # Import convenience functions from controllers
+      import Phoenix.Controller,
+        only: [get_flash: 1, get_flash: 2, view_module: 1, view_template: 1]
+
+      use Phoenix.HTML
+    end
+  end
+
   def router do
     quote do
       use Phoenix.Router
