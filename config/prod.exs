56da2e1ea9b6613969b880d81e9e44c5a5c28cc5
Paul Kim <pkim@mbta.com>
2022-10-19 17:43:59 -0400

    Use awscli credentials

--- config/prod.exs
+++ config/prod.exs
@@ -18,3 +18,9 @@ config :realtime_signs,
   external_config_getter: ExternalConfig.S3,
   sign_updater_mod: MessageQueue,
   restart_fn: &System.restart/0
+
+config :ex_aws,
+  access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, {:awscli, "default", 30}],
+  secret_access_key: [
+    {:system, "AWS_SECRET_ACCESS_KEY"},
+    {:awscli, "default", 30}
