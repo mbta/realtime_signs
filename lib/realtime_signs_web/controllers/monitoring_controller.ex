2a00182b0145734b1e0ec39d9735b612bb9d61a0
Kim <pkim@mbta.com>
2023-03-17 16:04:32 -0400

    Add controller method and routing

--- lib/realtime_signs_web/controllers/monitoring_controller.ex
+++ lib/realtime_signs_web/controllers/monitoring_controller.ex
@@ -1,14 +1,15 @@
 defmodule RealtimeSignsWeb.MonitoringController do
   require Logger
   use RealtimeSignsWeb, :controller
-  import Monitoring.Uptime
+  alias Monitoring.Headend
+  alias Monitoring.Uptime
 
   def uptime(
         conn,
         %{"time" => timestamp, "data" => %{"nodes" => nodes}} = _params
       ) do
     Logger.info("Received uptime statuses from ARINC: Node count=#{Enum.count(nodes)}")
-    monitor_node_uptime(nodes, timestamp)
+    Uptime.monitor_node_uptime(nodes, timestamp)
     send_resp(conn, 200, "")
   end
 
@@ -38,5 +39,21 @@ defmodule RealtimeSignsWeb.MonitoringController do
     )
 
     send_resp(conn, 200, "")
+  end
+
+  def update_active_headend(conn, %{"active_ip" => active_ip} = _params) do
+    case Headend.update_active_headend_ip(active_ip) do
+      {:ok, _} ->
+        Logger.info("active_headend_ip: Updated to #{active_ip}")
+        send_resp(conn, 200, "ok")
+
+      {:bad_request, message} ->
+        Logger.warn("active_headend_ip: Failed to update to #{active_ip}")
+        send_resp(conn, 400, message)
+
+      {:error, _} ->
+        Logger.warn("active_headend_ip: Failed to update to #{active_ip}")
+        send_resp(conn, 500, "Internal server error")
+    end
   end
 end
