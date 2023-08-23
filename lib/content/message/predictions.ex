3a70dc4314786b8bff5ef4cb14dcf94736bc9286
Kim <pkim@mbta.com>
2023-08-23 15:58:14 -0400

    Add main crowding determination logic

--- lib/content/message/predictions.ex
+++ lib/content/message/predictions.ex
@@ -32,7 +32,8 @@ defmodule Content.Message.Predictions do
     new_cars?: false,
     terminal?: false,
     certainty: nil,
-    crowding_data_confidence: nil
+    crowding_data_confidence: nil,
+    crowding_description: nil
   ]
 
   @type t :: %__MODULE__{
@@ -49,8 +50,54 @@ defmodule Content.Message.Predictions do
           platform: Content.platform() | nil,
           terminal?: boolean(),
           certainty: non_neg_integer() | nil,
-          crowding_data_confidence: :high | :low | nil
+          crowding_data_confidence: :high | :low | nil,
+          crowding_description: {atom(), atom()} | nil
         }
+
+  # Zone definitions based on number of emptier cars
+  # - Tuple represents a range of car numbers (inclusive)
+  # - List represents a specific set of car numbers that define the zone
+  # - If zone is nil, it's not relevant for that number of cars
+  @crowding_zone_definitions %{
+    1 => [
+      front: {1, 2},
+      back: {5, 6},
+      middle: {3, 4},
+      train_level: nil
+    ],
+    2 => [
+      front: {1, 3},
+      back: {4, 6},
+      middle: [[3, 4], [2, 4], [3, 5]],
+      train_level: nil
+    ],
+    3 => [
+      middle: {2, 5},
+      front: {1, 4},
+      back: {3, 6},
+      train_level: [[1, 3, 5], [2, 4, 6]]
+    ],
+    4 => [
+      middle: {2, 5},
+      front: {1, 5},
+      back: {2, 6},
+      train_level: [[1, 3, 4, 6], [1, 2, 4, 6], [1, 3, 5, 6]]
+    ],
+    5 => [
+      front: {1, 5},
+      back: {2, 6},
+      middle: nil,
+      train_level: [[1, 3, 4, 5, 6], [1, 2, 4, 5, 6], [1, 2, 3, 5, 6], [1, 2, 3, 4, 6]]
+    ],
+    6 => [
+      train_level: {1, 6}
+    ]
+  }
+
+  @not_crowded 1
+  @some_crowding 2
+  @crowded 3
+  @unknown_crowding 4
 
   @spec non_terminal(
           Predictions.Prediction.t(),
@@ -79,13 +126,7 @@ defmodule Content.Message.Predictions do
         true -> predicted_time |> Kernel./(60) |> round()
       end
 
-    crowding_data_confidence =
-      calculate_crowding_data_confidence(
-        prediction,
-        Engine.Locations.for_vehicle(prediction.vehicle_id)
-      )
-
-    # TODO: Calculate crowding data classification and pass that along as well
+    {crowding_data_confidence, crowding_description} = do_crowding(prediction)
 
     case Content.Utilities.destination_for_prediction(
            prediction.route_id,
@@ -106,7 +147,8 @@ defmodule Content.Message.Predictions do
           zone: zone,
           platform: platform,
           certainty: certainty,
-          crowding_data_confidence: crowding_data_confidence
+          crowding_data_confidence: crowding_data_confidence,
+          crowding_description: crowding_description
         }
 
       {:error, _} ->
@@ -154,16 +196,100 @@ defmodule Content.Message.Predictions do
     end
   end
 
-  defp calculate_crowding_data_confidence(_prediction, nil), do: nil
+  defp do_crowding(prediction) when prediction.route_id in ["Orange"] do
+    case Engine.Locations.for_vehicle(prediction.vehicle_id) do
+      %Locations.Location{} = location ->
+        {calculate_crowding_data_confidence(prediction, location),
+         get_crowding_description(location.multi_carriage_details)}
+
+      _ ->
+        {nil, nil}
+    end
+  end
+
+  defp do_crowding(_), do: {nil, nil}
 
   defp calculate_crowding_data_confidence(prediction, location)
-       when prediction.route_id in ["Orange"] and location.stop_id == prediction.stop_id do
+       when location.stop_id == prediction.stop_id do
     if location.status in [:incoming_at, :in_transit_to],
       do: :high,
       else: :low
   end
 
   defp calculate_crowding_data_confidence(_prediction, _location), do: nil
+
+  defp get_crowding_description([]), do: nil
+
+  defp get_crowding_description(carriage_details) do
+    crowding_level_by_car =
+      Enum.map(carriage_details, fn carriage ->
+        {carriage.carriage_sequence,
+         occupancy_status_to_crowding_level(carriage.occupancy_status)}
+      end)
+
+    min_crowding_level = Enum.min_by(crowding_level_by_car, &elem(&1, 1)) |> elem(1)
+
+    emptier_car_nums =
+      Stream.map(crowding_level_by_car, fn {car_num, crowding_level} ->
+        if crowding_level > min_crowding_level,
+          do: {car_num, :fuller},
+          else: {car_num, :emptier}
+      end)
+      |> Stream.filter(&match?({_, :emptier}, &1))
+      |> Enum.map(&elem(&1, 0))
+
+    emptier_location =
+      Enum.reduce_while(
+        @crowding_zone_definitions[Enum.count(emptier_car_nums)],
+        :front_and_back,
+        fn {zone, definition}, acc ->
+          if in_zone?(emptier_car_nums, zone, definition),
+            do: {:halt, zone},
+            else: {:cont, acc}
+        end
+      )
+
+    {emptier_location, crowding_level_to_atom(min_crowding_level)}
+  end
+
+  defp occupancy_status_to_crowding_level(occupancy_status) do
+    case occupancy_status do
+      :many_seats_available -> @not_crowded
+      :few_seats_available -> @not_crowded
+      :standing_room_only -> @some_crowding
+      :crushed_standing_room_only -> @crowded
+      :full -> @crowded
+      _ -> @unknown_crowding
+    end
+  end
+
+  defp crowding_level_to_atom(crowding_level) do
+    case crowding_level do
+      @not_crowded -> :not_crowded
+      @some_crowding -> :some_crowding
+      @crowded -> :crowded
+      @unknown_crowding -> :unknown_croding
+    end
+  end
+
+  defp in_zone?(_, _, nil), do: false
+
+  defp in_zone?(car_nums, zone, {front_end, back_end}) do
+    special_case? =
+      (Enum.count(car_nums) == 2 and zone == :front and car_nums == [1, 4]) or
+        (Enum.count(car_nums) == 2 and zone == :back and car_nums == [3, 6]) or
+        (Enum.count(car_nums) == 4 and zone == :front and car_nums == [1, 2, 3, 6]) or
+        (Enum.count(car_nums) == 4 and zone == :back and car_nums == [1, 4, 5, 6])
+
+    special_case? or
+      Enum.all?(car_nums, fn car_num -> car_num in Enum.to_list(front_end..back_end) end)
+  end
+
+  defp in_zone?(car_nums, _zone, zone_patterns) when is_list(zone_patterns) do
+    Enum.any?(zone_patterns, fn pattern ->
+      car_nums == pattern
+    end)
+  end
 
   defimpl Content.Message do
     require Logger
