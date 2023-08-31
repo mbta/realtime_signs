740f0c1c618cbf3636c654a0f1053e098f3ba4fd
Paul Kim <pkim@mbta.com>
2023-08-31 11:49:57 -0400

    Determine emptier car location and crowding description (#677)
    * Add locations feed engine
    
    * Add unit test for 'deletion' logic
    
    * Add a few more tests
    
    * remmove io puts
    
    * fix typedef
    
    * Begin reading in multi_carriage_details field
    
    * Calculate crowding data confidence level
    
    * Branch arrival and approaching audio logic based on crowding data confidence level
    
    * Add a todo for where to incorporate read loop selection logic
    
    * Clean up calculation logic
    
    * adjust mapping logic to match actual schema
    
    * Add main crowding determination logic
    
    * Start passing crowding description to Arriving and Approaching messages
    
    * Fix merge mistakes
    
    * Address PR comments
    
    * Condense 2 car cases

--- lib/content/audio/predictions.ex
+++ lib/content/audio/predictions.ex
@@ -46,51 +46,34 @@ defmodule Content.Audio.Predictions do
         ]
 
       predictions.minutes == :arriving ->
-        if predictions.crowding_data_confidence == :high do
-          # TODO: Pass along crowding data classification once available
-          [
-            %TrainIsArriving{
-              destination: predictions.destination,
-              trip_id: predictions.trip_id,
-              platform: predictions.platform,
-              route_id: predictions.route_id
-            }
-          ]
-        else
-          [
-            %TrainIsArriving{
-              destination: predictions.destination,
-              trip_id: predictions.trip_id,
-              platform: predictions.platform,
-              route_id: predictions.route_id
-            }
-          ]
-        end
+        [
+          %TrainIsArriving{
+            destination: predictions.destination,
+            trip_id: predictions.trip_id,
+            platform: predictions.platform,
+            route_id: predictions.route_id,
+            crowding_description:
+              if(predictions.crowding_data_confidence == :high,
+                do: predictions.crowding_description
+              )
+          }
+        ]
 
       predictions.minutes == :approaching and (line == :top or multi_source?) and
           predictions.route_id in @heavy_rail_routes ->
-        if predictions.crowding_data_confidence == :high do
-          # TODO: Pass along crowding data classification once available
-          [
-            %Approaching{
-              destination: predictions.destination,
-              trip_id: predictions.trip_id,
-              platform: predictions.platform,
-              route_id: predictions.route_id,
-              new_cars?: predictions.new_cars?
-            }
-          ]
-        else
-          [
-            %Approaching{
-              destination: predictions.destination,
-              trip_id: predictions.trip_id,
-              platform: predictions.platform,
-              route_id: predictions.route_id,
-              new_cars?: predictions.new_cars?
-            }
-          ]
-        end
+        [
+          %Approaching{
+            destination: predictions.destination,
+            trip_id: predictions.trip_id,
+            platform: predictions.platform,
+            route_id: predictions.route_id,
+            new_cars?: predictions.new_cars?,
+            crowding_description:
+              if(predictions.crowding_data_confidence == :high,
+                do: predictions.crowding_description
+              )
+          }
+        ]
 
       predictions.minutes == :approaching ->
         [
