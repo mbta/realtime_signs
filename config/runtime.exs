5ca726c731c784a284236313f5345adbc93cca49
Kim <pkim@mbta.com>
2023-09-14 12:18:23 -0400

    Make headend path an env var and add case for setting active headend as nil

--- config/runtime.exs
+++ config/runtime.exs
@@ -24,6 +24,7 @@ if config_env() != :test do
     message_log_s3_folder: System.get_env("MESSAGE_LOG_S3_FOLDER"),
     message_log_report_s3_folder: System.get_env("MESSAGE_LOG_REPORT_S3_FOLDER"),
     monitoring_api_key: System.get_env("MONITORING_API_KEY")
+    active_headend_path: System.get_env("ACTIVE_HEADEND_S3_PATH", "/headend.json"),
 end
 
 if config_env() == :dev do
