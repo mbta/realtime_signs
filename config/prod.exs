a30245244e56210f7fc574e97efefd1c25015aeb
Paul Kim <pkim@mbta.com>
2024-03-14 14:17:13 -0400

    Revert "Update dockerfile and actions (#732)" (#739)
    This reverts commit dc625ff2dfb78288b9c1614b73f07c306a1fc8f3.

--- config/prod.exs
+++ config/prod.exs
@@ -19,9 +19,8 @@ config :realtime_signs,
   restart_fn: &System.restart/0
 
 config :ex_aws,
-  access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, {:awscli, "default", 30}, :instance_role],
+  access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, {:awscli, "default", 30}],
   secret_access_key: [
     {:system, "AWS_SECRET_ACCESS_KEY"},
-    {:awscli, "default", 30},
-    :instance_role
+    {:awscli, "default", 30}
   ]
