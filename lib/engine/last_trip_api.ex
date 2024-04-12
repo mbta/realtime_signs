e6a01d67067d03ce9be7d8bff57b1136bac8b366
Kim <pkim@mbta.com>
2024-04-12 09:37:47 -0400

    dialyzer

--- lib/engine/last_trip_api.ex
+++ lib/engine/last_trip_api.ex
@@ -1,4 +1,4 @@
 defmodule Engine.LastTripAPI do
-  @callback get_recent_departures(String.t()) :: [Map.t()]
+  @callback get_recent_departures(String.t()) :: map()
   @callback is_last_trip?(String.t()) :: boolean()
 end
