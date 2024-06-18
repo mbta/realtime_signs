63f72ecf2ed96cd7c6f50210f710669462915a77
Kim <pkim@mbta.com>
2024-06-18 10:40:38 -0400

    Put active messages path only in application env

--- config/runtime.exs
+++ config/runtime.exs
@@ -5,6 +5,9 @@ config :realtime_signs, RealtimeSignsWeb.Endpoint,
   url: [host: "localhost"],
   server: true
 
+config :realtime_signs,
+  screenplay_base_url: System.get_env("SCREENPLAY_BASE_URL")
+
 if config_env() != :test do
   config :realtime_signs,
     sign_ui_url: System.get_env("SIGN_UI_URL"),
@@ -39,13 +42,13 @@ if config_env() != :test do
     message_log_report_s3_folder: System.get_env("MESSAGE_LOG_REPORT_S3_FOLDER"),
     monitoring_api_key: System.get_env("MONITORING_API_KEY"),
     active_headend_path: System.get_env("ACTIVE_HEADEND_S3_PATH"),
-    active_pa_messages_url: System.get_env("ACTIVE_PA_MESSAGES_URL"),
     screenplay_api_key: System.get_env("SCREENPLAY_API_KEY")
 end
 
 if config_env() == :dev do
   config :realtime_signs,
-    sign_config_file: System.get_env("SIGN_CONFIG_FILE")
+    sign_config_file: System.get_env("SIGN_CONFIG_FILE"),
+    screenplay_base_url: System.get_env("SCREENPLAY_BASE_URL")
 end
 
 message_log_job =
