e959cec7886030ac72eebeca33723669d24150f3
Brett Heath-Wlaz <bheathwlaz@mbta.com>
2024-07-01 12:06:29 -0400

    remove phoenix_view and phoenix_html (#793)

--- mix.exs
+++ mix.exs
@@ -56,9 +56,6 @@ defmodule RealtimeSigns.Mixfile do
       {:uuid, "~> 1.1", only: :test},
       {:quantum, "~> 3.0"},
       {:phoenix, "~> 1.7.0"},
-      {:phoenix_html, "~> 3.0"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_view, "~> 2.0"},
       {:plug_cowboy, "~> 2.5"},
       {:configparser_ex, "~> 4.0", only: [:prod]},
       {:remote_ip, "~> 1.2"}
