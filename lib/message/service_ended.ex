14fd55c1d2c33727309e41ed53499f1518157b1b
Brett Heath-Wlaz <bheathwlaz@mbta.com>
2025-01-16 13:36:03 -0500

    introduce Message structs

---
+++ lib/message/service_ended.ex
@@ -0,0 +1,27 @@
+defmodule Message.ServiceEnded do
+  @enforce_keys [:route, :destination]
+  defstruct @enforce_keys
+
+  @type t :: %__MODULE__{
+          destination: PaEss.destination() | nil,
+          route: String.t() | nil
+        }
+
+  defimpl Message do
+    def to_single_line(%Message.ServiceEnded{route: route, destination: destination}) do
+      %Content.Message.LastTrip.NoService{destination: destination, route: route}
+    end
+
+    def to_full_page(%Message.ServiceEnded{destination: nil, route: route}) do
+      {%Content.Message.LastTrip.StationClosed{route: route},
+       %Content.Message.LastTrip.ServiceEnded{destination: nil}}
+    end
+
+    def to_full_page(%Message.ServiceEnded{destination: destination}) do
+      {%Content.Message.LastTrip.PlatformClosed{destination: destination},
+       %Content.Message.LastTrip.ServiceEnded{destination: destination}}
+    end
+
+    def to_multi_line(%Message.ServiceEnded{} = message), do: to_full_page(message)
+  end
+end
