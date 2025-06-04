942bc8d3b4da9f7d058adb9ea88e06c2816d5c3b
Kim <pkim@mbta.com>
2025-06-04 14:15:19 -0400

    Update track change

--- lib/content/audio/track_change.ex
+++ lib/content/audio/track_change.ex
@@ -15,16 +15,36 @@ defmodule Content.Audio.TrackChange do
         }
 
   @spec park_track_change?(Predictions.Prediction.t()) :: boolean()
-  def park_track_change?(%{route_id: "Green-B", stop_id: "70197"}), do: true
-  def park_track_change?(%{route_id: "Green-C", stop_id: "70196"}), do: true
-  def park_track_change?(%{route_id: "Green-D", stop_id: "70199"}), do: true
-  def park_track_change?(%{route_id: "Green-E", stop_id: "70198"}), do: true
+  def park_track_change?(%{route_id: "Green-B", stop_id: "70198"}), do: true
+  def park_track_change?(%{route_id: "Green-C", stop_id: "70199"}), do: true
+  def park_track_change?(%{route_id: "Green-D", stop_id: "70196"}), do: true
+  def park_track_change?(%{route_id: "Green-E", stop_id: "70197"}), do: true
   def park_track_change?(_prediction), do: false
 
   defimpl Content.Audio do
     def to_params(%{route_id: route_id, berth: berth, destination: destination}) do
+      branch = Content.Utilities.route_branch_letter(route_id)
+
+      platform =
+        case berth do
+          "70196" -> :b
+          "70197" -> :c
+          "70198" -> :d
+          "70199" -> :e
+        end
+
       PaEss.Utilities.audio_message(
-        [:track_change, {:boarding, route_id, berth, destination}],
+        [
+          :track_change,
+          :the_next,
+          branch,
+          :train_to,
+          destination,
+          :is_now_boarding,
+          :on_the,
+          platform,
+          :platform
+        ],
         :audio_visual
       )
     end
