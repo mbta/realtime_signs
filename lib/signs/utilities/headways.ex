f99a87c725b965bd88ab3152836313ca8f9447a5
robbie.sundstrom <rsundstrom@mbta.com>
2025-06-20 19:30:26 -0400

    feat: Silver Line headways for platform signs

--- lib/signs/utilities/headways.ex
+++ lib/signs/utilities/headways.ex
@@ -1,14 +1,18 @@
 defmodule Signs.Utilities.Headways do
   @moduledoc """
   Given a sign with a SourceConfig, fetches headways and
-  geneartes the top and bottom lines for the sign
+  generates the top and bottom lines for the sign
   """
   alias Signs.Utilities.SourceConfig
   require Logger
 
   @spec headway_message(SourceConfig.config(), DateTime.t()) :: Message.t() | nil
   def headway_message(config, current_time) do
-    case fetch_headways(config.headway_group, config.sources, current_time) do
+    case fetch_headways(
+           config.headway_group,
+           Enum.map(config.sources, & &1.stop_id),
+           current_time
+         ) do
       nil ->
         nil
 
@@ -21,9 +25,24 @@ defmodule Signs.Utilities.Headways do
     end
   end
 
-  defp fetch_headways(headway_group, sources, current_time) do
-    stop_ids = Enum.map(sources, & &1.stop_id)
+  # Process headways for Silver Line differently b/c of its different config shape
+  @spec headway_message_sl(String.t(), String.t(), [String.t()], DateTime.t()) ::
+          Message.t() | nil
+  def headway_message_sl(headway_group, headway_destination, stop_ids, current_time) do
+    case fetch_headways(headway_group, stop_ids, current_time) do
+      nil ->
+        nil
 
+      headways ->
+        %Message.Headway{
+          destination: headway_destination,
+          range: {headways.range_low, headways.range_high},
+          route: "Silver"
+        }
+    end
+  end
+
+  defp fetch_headways(headway_group, stop_ids, current_time) do
     with headways when not is_nil(headways) <-
            RealtimeSigns.config_engine().headway_config(headway_group, current_time),
          true <-
