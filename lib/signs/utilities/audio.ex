91873eb8d6f4f8dc0eb8ab55cc8e0d70f2f9cfe0
Ryan Waskiewicz <rwaskiewicz@mbta.com>
2026-05-18 20:59:39 -0400

    add visual text to logging

--- lib/signs/utilities/audio.ex
+++ lib/signs/utilities/audio.ex
@@ -229,12 +229,16 @@ defmodule Signs.Utilities.Audio do
 
   @spec log_send_pa_message(PaMessages.PaMessage.t(), Signs.Realtime.t() | Signs.Bus.t()) :: :ok
   defp log_send_pa_message(pa_message, sign) do
-    Logger.info("pa_message: action=send id=#{pa_message.id} destination=#{sign.id}")
+    Logger.info(
+      "pa_message: action=send id=#{pa_message.id} destination=#{sign.id} visual_text=#{pa_message.visual_text}"
+    )
   end
 
   @spec log_skipped_pa_message(PaMessages.PaMessage.t(), Signs.Realtime.t() | Signs.Bus.t()) ::
           :ok
   defp log_skipped_pa_message(pa_message, sign) do
-    Logger.warning("pa_message: action=skipped id=#{pa_message.id} destination=#{sign.id}")
+    Logger.warning(
+      "pa_message: action=skipped id=#{pa_message.id} destination=#{sign.id} visual_text=#{pa_message.visual_text}"
+    )
   end
 end
