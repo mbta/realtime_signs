625eb0632407e5c9cf0c8f9c889110b2404fd5b6
Brett Heath-Wlaz <bheathwlaz@mbta.com>
2026-05-27 06:43:11 -0700

    play PA messages per-screen (#1027)

--- test/engine/pa_messages_test.exs
+++ test/engine/pa_messages_test.exs
@@ -2,10 +2,6 @@ defmodule Engine.PaMessagesTest do
   use ExUnit.Case
   import Mox
 
-  @state %{
-    pa_messages_last_sent: %{}
-  }
-
   setup_all do
     screenplay_base_url = Application.get_env(:realtime_signs, :screenplay_base_url)
     active_pa_messages_path = Application.get_env(:realtime_signs, :active_pa_messages_path)
@@ -30,39 +26,24 @@ defmodule Engine.PaMessagesTest do
       :active_pa_messages_path,
       "/api/pa-messages/active"
     )
+
+    table = :ets.new(:pa_messages_test, read_concurrency: true)
+    %{table: table}
   end
 
   describe "handle_info/2" do
-    test "Plays PA messages" do
-      {:noreply, state} = Engine.PaMessages.handle_info(:update, @state)
+    test "fetches PA messages", %{table: table} do
+      Engine.PaMessages.handle_info(:update, %{table: table})
 
-      pa_ids = Map.keys(state.pa_messages_last_sent)
-
-      assert 4 in pa_ids
-      assert 5 in pa_ids
+      assert [%{id: 4}] = Engine.PaMessages.for_sign("1", table)
+      assert [%{id: 5}] = Engine.PaMessages.for_sign("2", table)
     end
 
-    test "Doesn't play PA Message if less than interval has passed" do
-      last_played = DateTime.utc_now() |> DateTime.add(-1, :minute)
+    test "Ignores inactive PA messages", %{table: table} do
+      Engine.PaMessages.handle_info(:update, %{table: table})
 
-      state = %{
-        pa_messages_last_sent: %{
-          4 => last_played,
-          5 => last_played
-        }
-      }
-
-      {:noreply, state} = Engine.PaMessages.handle_info(:update, state)
-      assert last_played == Map.get(state.pa_messages_last_sent, 4)
-      assert last_played == Map.get(state.pa_messages_last_sent, 5)
-    end
-
-    test "Ignores inactive PA messages" do
-      {:noreply, state} = Engine.PaMessages.handle_info(:update, @state)
-      pa_ids = Map.keys(state.pa_messages_last_sent)
-
-      assert 4 in pa_ids
-      assert 5 in pa_ids
+      assert [%{id: 4}] = Engine.PaMessages.for_sign("1", table)
+      assert [%{id: 5}] = Engine.PaMessages.for_sign("2", table)
 
       Application.put_env(
         :realtime_signs,
@@ -70,25 +51,10 @@ defmodule Engine.PaMessagesTest do
         "/api/pa-messages/no-longer-active"
       )
 
-      {:noreply, state} = Engine.PaMessages.handle_info(:update, @state)
-      pa_ids = Map.keys(state.pa_messages_last_sent)
+      Engine.PaMessages.handle_info(:update, %{table: table})
 
-      refute 4 in pa_ids
-      assert 5 in pa_ids
-    end
-
-    test "Plays PA Message when interval is edited to be shorter" do
-      last_play = DateTime.utc_now() |> DateTime.add(-2, :minute)
-      state = %{pa_messages_last_sent: %{5 => last_play}}
-
-      Application.put_env(
-        :realtime_signs,
-        :active_pa_messages_path,
-        "/api/pa-messages/changed-interval"
-      )
-
-      {:noreply, state} = Engine.PaMessages.handle_info(:update, state)
-      refute last_play == Map.get(state.pa_messages_last_sent, 5)
+      assert [] = Engine.PaMessages.for_sign("1", table)
+      assert [%{id: 5}] = Engine.PaMessages.for_sign("2", table)
     end
   end
 end
