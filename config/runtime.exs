4fa487829f5e686b4db8a5e0d88cfdb59ccc6b71
Brett Heath-Wlaz <bheathwlaz@mbta.com>
2026-08-11 12:27:55 -0400

    remove splunk logger backend (#1070)

--- config/runtime.exs
+++ config/runtime.exs
@@ -77,16 +77,3 @@ if config_env() == :prod do
   config :realtime_signs, RealtimeSignsWeb.Endpoint,
     secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
 end
-
-# For maintaining backwards compatibility with opstech3
-splunk_token = System.get_env("PROD_SIGNS_SPLUNK_TOKEN", "")
-
-if splunk_token != "" do
-  config :logger, :splunk,
-    connector: Logger.Backend.Splunk.Output.Http,
-    host: "https://http-inputs-mbta.splunkcloud.com/services/collector/event",
-    token: splunk_token,
-    format: "$dateT$time [$level] node=$node $metadata$message\n",
-    metadata: [:request_id],
-    max_buffer: 100
-end
