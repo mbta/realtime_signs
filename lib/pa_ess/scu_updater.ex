1edcc189bf4783ffc5bd979c9deabf798b8d273e
Brett Heath-Wlaz <bheathwlaz@mbta.com>
2026-08-24 12:27:20 -0400

    shorten SCU timeout

--- lib/pa_ess/scu_updater.ex
+++ lib/pa_ess/scu_updater.ex
@@ -42,7 +42,8 @@ defmodule PaEss.ScuUpdater do
     if address do
       http_poster.post("http://#{address}#{path}",
         headers: [x_api_key: scully_api_key],
-        json: body
+        json: body,
+        receive_timeout: 2000
       )
       |> case do
         {:ok, %Req.Response{status: status}} when status in 200..299 ->
